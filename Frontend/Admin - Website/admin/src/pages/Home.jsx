dd79f0a015d7a82e75d2b2ae772ac70319b20cae
Marc Edison <suanieeee@yahoo.com>
2024-02-07 21:39:41 +0800

    Add Tailwind CSS and PostCSS configuration files

--- Frontend/Admin - Website/admin/src/pages/Home.jsx
+++ Frontend/Admin - Website/admin/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full h-screen text-[60px] flex justify-center items-center text-center'>
+      Rent and share platform <br />&<br /> Circular Fashion Marketplace
+    </div>
   )
 }
 
