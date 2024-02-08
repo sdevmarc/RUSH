3e5a724e2707668a49bba15a6bf42619562a469c
Marc Edison <suanieeee@yahoo.com>
2024-02-08 23:17:13 +0800

    Refactor Header component layout and styles

--- Frontend/Admin - Website/admin/src/components/Home/Header.jsx
+++ Frontend/Admin - Website/admin/src/components/Home/Header.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <div className="w-full h-[8vh] bg-black flex justify-between items-center px-[15rem]">
-                <div className="overflow-hidden w-[2rem] h-[2rem] bg-yellow-500">
+            <div className="w-full h-[8vh] flex flex-col px-[15rem]">
+                <div className="w-full h-full flex justify-between items-center border-b-black border-b border-solid">
                     <h1
-                        className='w-full h-full flex justify-center items-center text-[13px]'
-                    >Logo</h1>
+                        className='text-[20px] font-semibold'
+                    >
+                        Logo
+                    </h1>
+                    <h1
+                        className='text-[15px] font-semibold cursor-pointer'
+                    >
+                        Download
+                    </h1>
+                </div>
+                <div className="w-full h-[5vh] flex justify-center items-center bg-[#e7faff]">
+                    <h1
+                        className='text-[13px] font-semibold'
+                    >Early access is now live!</h1>
                 </div>
             </div>
         </>
