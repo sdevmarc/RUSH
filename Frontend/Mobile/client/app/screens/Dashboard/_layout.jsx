383461f6bb1ac2c0c89725be3658f84776ba3de7
sdevmarc <suanieeee@yahoo.com>
2024-02-22 10:21:50 +0800

    Changed routes directing to home

---
+++ Frontend/Mobile/client/app/screens/Dashboard/_layout.jsx
@@ -0,0 +1,32 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { StyleSheet } from 'react-native';
+
+
+const _layout = () => {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
+                <Drawer.Screen
+                    name="Home"
+                    options={{
+                        drawerLabel: 'Home',
+                        title: 'Home',
+                    }}
+                />
+            </Drawer>
+        </GestureHandlerRootView>
+    )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({
+
+}
+
+)
