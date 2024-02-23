d2c5058bc2ab1a9da54fa14e126605210b40992b
Marc Edison <suanieeee@yahoo.com>
2024-02-23 11:42:03 +0800

    Added Selected Item Screen

---
+++ Frontend/Mobile/client/app/screens/Dashboard/SelectedItem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectedItem = () => {
+  return (
+    <View>
+      <Text>SelectedItem</Text>
+    </View>
+  )
+}
+
+export default SelectedItem
