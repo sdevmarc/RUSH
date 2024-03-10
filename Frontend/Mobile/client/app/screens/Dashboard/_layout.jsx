9dc19980d2e9c9a8498b47cc268fe11a86b1ceab
Marc Edison <suanieeee@yahoo.com>
2024-03-10 19:27:11 +0800

    Change background color of drawer

--- Frontend/Mobile/client/app/screens/Dashboard/_layout.jsx
+++ Frontend/Mobile/client/app/screens/Dashboard/_layout.jsx
@@ -6,17 +6,20 @@ import { StyleSheet } from 'react-native';
 const _layout = () => {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer screenOptions={{ headerShown: false }} >
+            <Drawer
+                screenOptions=
+                {{
+                    headerShown: false,
+                    drawerActiveBackgroundColor: 'white',
+                    drawerActiveTintColor: 'black',
+                    drawerInactiveTintColor: 'white',
+                    drawerStyle: {
+                        backgroundColor: '#EE3C3C'
+                    },
+                }} >
                 <Drawer.Screen name="Home" options={{ drawerLabel: 'Home', title: 'Home' }} />
             </Drawer>
         </GestureHandlerRootView>
     )
 }
-
 export default _layout
-
-const styles = StyleSheet.create({
-
-}
-
-)
