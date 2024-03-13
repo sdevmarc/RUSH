f0e7a9491e848b9892a186b5618c7f3e51cba514
Marc Edison <suanieeee@yahoo.com>
2024-03-14 03:19:52 +0800

    change it to signup only

--- Backend/server/routes/SignUp.js
+++ Backend/server/routes/SignUp.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const SignUp = require('../controllers/SignUp')
+const SignUp = require('../controllers/Users')
+const Login = require('../middleware/Login')
 
 router.post('/signup', SignUp.CreateUser)
 
