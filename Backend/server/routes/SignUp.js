cc6847376fd58489aa6e3d19a3079ace3f3da916
Marc Edison <suanieeee@yahoo.com>
2024-03-14 03:22:34 +0800

    Remove unused import in SignUp.js

--- Backend/server/routes/SignUp.js
+++ Backend/server/routes/SignUp.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const SignUp = require('../controllers/Users')
-const Login = require('../middleware/Login')
 
 router.post('/signup', SignUp.CreateUser)
 
