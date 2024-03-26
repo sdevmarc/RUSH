a55c00e749471903ec0ced33ba434171adb0ee79
Marc Edison <suanieeee@yahoo.com>
2024-03-26 19:28:09 +0800

    Added routes

--- Frontend/Mobile/reclient/app/Routes/Stack.jsx
+++ Frontend/Mobile/reclient/app/Routes/Stack.jsx
@@ -4,6 +4,8 @@ import Login from '../screens/Start/Login';
 import Register from '../screens/Start/Register';
 import Successful from '../screens/Start/Successful';
 import DrawerRoutes from './Drawer';
+import SelectedStore from '../screens/Dashboard/SelectedStore';
+import SelectedItem from '../screens/Dashboard/SelectedItem';
 
 
 const Stack = createNativeStackNavigator()
@@ -16,6 +18,8 @@ function StackRoutes() {
                 <Stack.Screen name="Register" component={Register} options={{ headerTitle: 'Register', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Successful" component={Successful} options={{ headerTitle: 'Successful', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="DrawerRoutes" component={DrawerRoutes} options={{ headerTitle: 'Home', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="SelectedStore" component={SelectedStore} options={{ headerTitle: 'Home', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="SelectedItem" component={SelectedItem} options={{ headerTitle: 'Home', headerTitleAlign: 'center', headerShown: false }} />
                 {/*  <Stack.Screen name="Login" component={Home} options={{ headerTitle: 'Login', headerTitleAlign: 'center', headerShown: false }} /> */}
             </Stack.Navigator>
         </NavigationContainer>
