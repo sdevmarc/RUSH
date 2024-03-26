e22352457ccfdc978ac123f665d578762bebd889
Marc Edison <suanieeee@yahoo.com>
2024-03-27 01:46:49 +0800

    Add new screens for Seller, Delivery Address, Payment Option, and Shipping Option

---
+++ Frontend/Mobile/reclient/app/screens/Settings/PaymentOption.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PaymentOption = () => {
+  return (
+    <View>
+      <Text>PaymentOption</Text>
+    </View>
+  )
+}
+
+export default PaymentOption
---
+++ Frontend/Mobile/reclient/app/screens/Settings/ShippingOption.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ShippingOption = () => {
+  return (
+    <View>
+      <Text>ShippingOption</Text>
+    </View>
+  )
+}
+
+export default ShippingOption
