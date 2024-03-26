3b46e6819dffcb8024d072e5374960e10cd4fcb8
Marc Edison <suanieeee@yahoo.com>
2024-03-27 01:54:50 +0800

    Update Drawer routes in Drawer.jsx

--- Frontend/Mobile/reclient/app/Routes/Drawer.jsx
+++ Frontend/Mobile/reclient/app/Routes/Drawer.jsx
@@ -4,11 +4,15 @@ import Home from '../screens/Dashboard/Home';
 const Drawer = createDrawerNavigator();
 
 const DrawerRoutes = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} options={{ headerTitle: 'Home', headerTitleAlign: 'center', headerShown: false }}/>
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={Home} options={{ drawerLabel: 'Home', headerTitle: 'Home', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="Account" component={Home} options={{drawerLabel: 'Account', headerTitle: 'Account', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="DeliverAddress" component={Home} options={{drawerLabel: 'Address', headerTitle: 'Address', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="Orders" component={Home} options={{drawerLabel: 'Orders', headerTitle: 'Orders', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="Settings" component={Home} options={{drawerLabel: 'Settings',headerTitle: 'Settings', headerTitleAlign: 'center', headerShown: false }} />
+        </Drawer.Navigator>
+    );
 }
 
 export default DrawerRoutes
