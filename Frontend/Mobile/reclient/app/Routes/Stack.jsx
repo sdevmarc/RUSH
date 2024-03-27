8c1579fc2dbc3b7e532488dd48553a1eb8e4a350
Marc Edison <suanieeee@yahoo.com>
2024-03-28 01:46:09 +0800

    Add new screens for delivery address, shipping option, and payment option

--- Frontend/Mobile/reclient/app/Routes/Stack.jsx
+++ Frontend/Mobile/reclient/app/Routes/Stack.jsx
@@ -8,6 +8,9 @@ import SelectedStore from '../screens/Dashboard/SelectedStore';
 import SelectedItem from '../screens/Dashboard/SelectedItem';
 import Cart from '../screens/Dashboard/Cart';
 import SuccessfulCheckout from '../screens/Dashboard/Successful';
+import DeliveryAddress from '../screens/Settings/DeliveryAddress';
+import ShippingOption from '../screens/Dashboard/ShippingOption';
+import PaymentOption from '../screens/Dashboard/PaymentOption';
 
 
 const Stack = createNativeStackNavigator()
@@ -16,14 +19,20 @@ function StackRoutes() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                {/* START */}
                 <Stack.Screen name="Login" component={Login} options={{ headerTitle: 'Login', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Register" component={Register} options={{ headerTitle: 'Register', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="SuccessfulLogin" component={SuccessfulLogin} options={{ headerTitle: 'Successful', headerTitleAlign: 'center', headerShown: false }} />
+                {/* DASHBOARD */}
                 <Stack.Screen name="DrawerRoutes" component={DrawerRoutes} options={{ headerTitle: 'Drawer', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="SelectedStore" component={SelectedStore} options={{ headerTitle: 'Store', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="SelectedItem" component={SelectedItem} options={{ headerTitle: 'Item', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Cart" component={Cart} options={{ headerTitle: 'Cart', headerTitleAlign: 'center', headerShown: false }} />
-                <Stack.Screen name="SuccessfulCheckout" component={SuccessfulCheckout} options={{ headerTitle: 'Cart', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="ShippingOption" component={ShippingOption} options={{ headerTitle: 'ShippingOption', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="PaymentOption" component={PaymentOption} options={{ headerTitle: 'ShippingOption', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="SuccessfulCheckout" component={SuccessfulCheckout} options={{ headerTitle: 'SuccessfulCheckout', headerTitleAlign: 'center', headerShown: false }} />
+                {/* ACCOUNT */}
+                <Stack.Screen name="Address" component={DeliveryAddress} options={{ headerTitle: 'Address', headerTitleAlign: 'center', headerShown: false }} />
                 {/*  <Stack.Screen name="Login" component={Home} options={{ headerTitle: 'Login', headerTitleAlign: 'center', headerShown: false }} /> */}
             </Stack.Navigator>
         </NavigationContainer>
