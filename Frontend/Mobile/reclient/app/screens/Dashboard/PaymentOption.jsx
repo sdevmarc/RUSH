795f4c16b2f9296fa7d3cf0642d3778779c52086
Marc Edison <suanieeee@yahoo.com>
2024-03-28 01:46:42 +0800

    Add payment method screen layout

--- Frontend/Mobile/reclient/app/screens/Dashboard/PaymentOption.jsx
+++ Frontend/Mobile/reclient/app/screens/Dashboard/PaymentOption.jsx
@@ -1,11 +1,54 @@
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  StatusBar
+} from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+const { width, height } = Dimensions.get('window')
 
 const PaymentOption = () => {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>PaymentOption</Text>
+    <>
+    <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+    <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
+        <View style={{ position: 'absolute', width: '100%', height: height * 0.1, overflow: 'hidden', zIndex: 1, backgroundColor: '#323d48', paddingVertical: height * 0.009 }}>
+            <View
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    paddingHorizontal: width * 0.05,
+                    width: width,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-end',
+                    zIndex: 1
+                }}
+            >
+                <TouchableOpacity onPress={() => navigation.goBack()} >
+                    <Ionicons name="chevron-back-circle" size={height * 0.04} color="#8f8f8f" />
+                </TouchableOpacity>
+            </View>
+            <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
+                <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
+                    Payment Method
+                </Text>
+            </View>
+        </View>
+        <ScrollView>
+            <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
+
+            </View>
+        </ScrollView>
     </View>
+</>
   )
 }
 
