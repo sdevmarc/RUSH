a577513e8a454d34e2f60ebba12b817f8aa9ba39
sdevmarc <suanieeee@yahoo.com>
2024-03-28 20:39:26 +0800

    Add Seller Store screen component

---
+++ Frontend/Mobile/reclient/app/screens/Seller/Store.jsx
@@ -0,0 +1,117 @@
+import {
+    View,
+    Text,
+    Dimensions,
+    StatusBar,
+    TouchableOpacity,
+    ScrollView
+} from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
+
+const { width, height } = Dimensions.get('window')
+
+const Store = () => {
+    const navigation = useNavigation()
+
+    return (
+        <>
+            <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+            <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
+                <View
+                    style={{
+                        position: 'absolute',
+                        width: '100%',
+                        height: height * 0.12,
+                        overflow: 'hidden',
+                        zIndex: 1,
+                        backgroundColor: '#323d48',
+                        paddingVertical: height * 0.009
+                    }}
+                >
+                    <View
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            paddingHorizontal: width * 0.05,
+                            width: width,
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'flex-end',
+                            zIndex: 1
+                        }}
+                    >
+                        <TouchableOpacity
+                            onPress={() => navigation.openDrawer()}
+                        >
+                            <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="white" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
+                            Renters Dashboard
+                        </Text>
+                    </View>
+                </View>
+                <ScrollView>
+                    <View style={{ width: width, height: height, paddingHorizontal: width * 0.03, paddingTop: height * 0.13 }}>
+                        <View style={{ width: '100%', height: '60%', gap: height * 0.01 }}>
+                            <TouchableOpacity
+                                style={{
+                                    height: '10%',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    paddingHorizontal: width * 0.03,
+                                    backgroundColor: '#4a4c59',
+                                    borderRadius: height * 0.01
+                                }}
+                            >
+                                <Text style={{ color: '#fff', fontWeight: 'bold' }}>
+                                    Manage Store Details
+                                </Text>
+                                <MaterialIcons name="arrow-forward-ios" size={width * 0.05} color="white" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{
+                                    height: '10%',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    paddingHorizontal: width * 0.03,
+                                    backgroundColor: '#4a4c59',
+                                    borderRadius: height * 0.01
+                                }}
+                            >
+                                <Text style={{ color: '#fff', fontWeight: 'bold' }}>
+                                    Manage Products
+                                </Text>
+                                <MaterialIcons name="arrow-forward-ios" size={width * 0.05} color="white" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{
+                                    height: '10%',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    paddingHorizontal: width * 0.03,
+                                    backgroundColor: '#4a4c59',
+                                    borderRadius: height * 0.01
+                                }}
+                            >
+                                <Text style={{ color: '#fff', fontWeight: 'bold' }}>
+                                    Manage Orders
+                                </Text>
+                                <MaterialIcons name="arrow-forward-ios" size={width * 0.05} color="white" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+        </>
+
+    )
+}
+
+export default Store
