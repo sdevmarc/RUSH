3169d319466ffc4ada937c9d0b335c15eb8a35ec
sdevmarc <suanieeee@yahoo.com>
2024-03-28 20:39:22 +0800

    Add Navbar component to ShippingOption screen

--- Frontend/Mobile/reclient/app/screens/Dashboard/ShippingOption.jsx
+++ Frontend/Mobile/reclient/app/screens/Dashboard/ShippingOption.jsx
@@ -9,6 +9,7 @@ import {
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
+import Navbar from '../../components/Navbar'
 
 const { width, height } = Dimensions.get('window')
 
@@ -19,29 +20,7 @@ const ShippingOption = () => {
         <>
             <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
             <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
-                <View style={{ position: 'absolute', width: '100%', height: height * 0.1, overflow: 'hidden', zIndex: 1, backgroundColor: '#323d48', paddingVertical: height * 0.009 }}>
-                    <View
-                        style={{
-                            width: '100%',
-                            height: '100%',
-                            paddingHorizontal: width * 0.05,
-                            width: width,
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            alignItems: 'flex-end',
-                            zIndex: 1
-                        }}
-                    >
-                        <TouchableOpacity onPress={() => navigation.goBack()} >
-                            <Ionicons name="chevron-back-circle" size={height * 0.04} color="#8f8f8f" />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
-                        <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
-                            Shipping Option
-                        </Text>
-                    </View>
-                </View>
+                <Navbar title='Shipping Option' backgroundColor='#323d48' />
                 <ScrollView>
                     <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
 
