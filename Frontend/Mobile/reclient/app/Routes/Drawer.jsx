9f484f7c3401115fdf7daef905c9cf10b137f0a5
sdevmarc <suanieeee@yahoo.com>
2024-03-28 20:38:52 +0800

    Update Drawer routes in Mobile app

--- Frontend/Mobile/reclient/app/Routes/Drawer.jsx
+++ Frontend/Mobile/reclient/app/Routes/Drawer.jsx
@@ -1,7 +1,9 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Dashboard/Home';
 import Account from '../screens/Settings/Account';
-import DeliveryAddress from '../screens/Settings/DeliveryAddress';
+import Store from '../screens/Seller/Store';
+import Orders from '../screens/Settings/Orders';
+import Settings from '../screens/Settings/Settings';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,9 +11,10 @@ const DrawerRoutes = () => {
     return (
         <Drawer.Navigator>
             <Drawer.Screen name="Home" component={Home} options={{ drawerLabel: 'Home', headerTitle: 'Home', headerTitleAlign: 'center', headerShown: false }} />
-            <Drawer.Screen name="Account" component={Account} options={{drawerLabel: 'Account', headerTitle: 'Account', headerTitleAlign: 'center', headerShown: false }} />
-            <Drawer.Screen name="Orders" component={Home} options={{drawerLabel: 'Orders', headerTitle: 'Orders', headerTitleAlign: 'center', headerShown: false }} />
-            <Drawer.Screen name="Settings" component={Home} options={{drawerLabel: 'Settings',headerTitle: 'Settings', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="StoreDashboard" component={Store} options={{ drawerLabel: 'Renters Dashboard', headerTitle: 'Account', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="Account" component={Account} options={{ drawerLabel: 'Account', headerTitle: 'Account', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="Orders" component={Orders} options={{ drawerLabel: 'Orders', headerTitle: 'Orders', headerTitleAlign: 'center', headerShown: false }} />
+            <Drawer.Screen name="Settings" component={Settings} options={{ drawerLabel: 'Settings', headerTitle: 'Settings', headerTitleAlign: 'center', headerShown: false }} />
         </Drawer.Navigator>
     );
 }
