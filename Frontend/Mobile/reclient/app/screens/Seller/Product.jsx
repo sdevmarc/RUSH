889256242fb3606342aa9f957bb21cbb2da93f5b
Marc Edison <suanieeee@yahoo.com>
2024-03-29 23:52:59 +0800

    Add Product screen component

---
+++ Frontend/Mobile/reclient/app/screens/Seller/Product.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const Product = () => {
+    return (
+        <>
+            <Navbar title='Products' backgroundColor='#323d48' />
+        </>
+    )
+}
+
+export default Product
