d0d94b54952d6953a6f90fdf0059162455f47b4a
sdevmarc <suanieeee@yahoo.com>
2024-03-31 01:56:44 +0800

    Add state and handlers for shop information inputs

--- Frontend/Mobile/reclient/app/screens/Seller/AddStore/ShopInformation.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/AddStore/ShopInformation.jsx
@@ -6,18 +6,45 @@ import {
     ScrollView,
     TextInput
 } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../../components/Navbar'
 import { useNavigation } from '@react-navigation/native'
 import BottomBar from '../../../components/BottomBar'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { width, height } = Dimensions.get('window')
 
 const ShopInformation = () => {
+    const [values, setValues] = useState({
+        shopName: '',
+        pickupAddress: '',
+        email: '',
+        mobileNumber: ''
+    })
+
     const navigation = useNavigation()
 
-    const handleNext = () => {
-        navigation.navigate('BusinessInformation')
+    const handleOnChangeShopName = (value) => {
+        setValues({ ...values, shopName: value })
+    }
+    const handleOnChangePickupAddress = (value) => {
+        setValues({ ...values, pickupAddress: value })
+    }
+    const handleOnChangeEmail = (value) => {
+        setValues({ ...values, email: value })
+    }
+    const handleOnChangeMobileNumber = (value) => {
+        setValues({ ...values, mobileNumber: value })
+    }
+
+    const handleNext = async () => {
+        try {
+            const dataShopInformation = JSON.stringify(values)
+            await AsyncStorage.setItem('shopInfo', dataShopInformation)
+            navigation.navigate('BusinessInformation')
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     return (
@@ -32,25 +59,33 @@ const ShopInformation = () => {
                                 <Text style={{ color: 'white', fontWeight: 'bold' }}>
                                     Shop Name
                                 </Text>
-                                <TextInput style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your shop name?' />
+                                <TextInput
+                                    onChangeText={handleOnChangeShopName}
+                                    style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your shop name?' />
                             </View>
                             <View style={{ width: '100%', gap: height * 0.01 }}>
                                 <Text style={{ color: 'white', fontWeight: 'bold' }}>
                                     Pickup Address
                                 </Text>
-                                <TextInput style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='Where is your shop located?' />
+                                <TextInput
+                                    onChangeText={handleOnChangePickupAddress}
+                                    style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='Where is your shop located?' />
                             </View>
                             <View style={{ width: '100%', gap: height * 0.01 }}>
                                 <Text style={{ color: 'white', fontWeight: 'bold' }}>
                                     Email
                                 </Text>
-                                <TextInput style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your email?' />
+                                <TextInput
+                                    onChangeText={handleOnChangeEmail}
+                                    style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your email?' />
                             </View>
                             <View style={{ width: '100%', gap: height * 0.01 }}>
                                 <Text style={{ color: 'white', fontWeight: 'bold' }}>
                                     Mobile Number
                                 </Text>
-                                <TextInput style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your mobile number?' />
+                                <TextInput
+                                    onChangeText={handleOnChangeMobileNumber}
+                                    style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your mobile number?' />
                             </View>
                         </View>
                     </View>
