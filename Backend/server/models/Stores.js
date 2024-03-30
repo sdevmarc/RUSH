203d5069148dc2f6acca1e0cb2d7951f38492e7a
sdevmarc <suanieeee@yahoo.com>
2024-03-31 01:57:29 +0800

    Refactor StoreSchema in Stores.js

--- Backend/server/models/Stores.js
+++ Backend/server/models/Stores.js
@@ -5,12 +5,33 @@ const StoreSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    storeName: {
-        type: String,
-        required: true
+    shopInformation:
+    {
+        shopName: String,
+        pickupAddress: String,
+        email: String,
+        mobileNumber: String
     },
-    storePhoto: {
-        type: String
+    businessInformation:
+    {
+        sellerType: String,
+        registeredName: {
+            lastname: String,
+            firstname: String,
+            middlename: String
+        },
+        registeredAddress: {
+            city: String,
+            province: String,
+            municipality: String,
+            barangay: String
+        },
+        taxIdentificationNumber: {
+            taxNumber: String,
+            certificateOfRegistration: String,
+            businessName: String
+        }
+
     },
     products: [
         {
@@ -19,7 +40,8 @@ const StoreSchema = new mongoose.Schema({
             productDescription: String,
             productPrice: String
         }
-    ]
+    ],
+    termsAndConditions: Boolean
 })
 
 const StoresModel = mongoose.model('Stores', StoreSchema)
