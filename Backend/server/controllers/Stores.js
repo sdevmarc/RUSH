fef1e1d56476c7a42814c5002eac961baf614ee3
sdevmarc <suanieeee@yahoo.com>
2024-03-31 01:57:10 +0800

    Refactor StoreController in Backend

--- Backend/server/controllers/Stores.js
+++ Backend/server/controllers/Stores.js
@@ -7,7 +7,16 @@ const StoreController = {
             await Stores.create(values)
             res.json({ success: true, message: 'Store added successfully' })
         } catch (error) {
-            res.json({ success: false, message: `Error Store Controller: ${error}` })
+            res.json({ success: false, message: `Error create store controller: ${error}` })
+        }
+    },
+    GetStore: async (req, res) => {
+        try {
+            const { userId } = req.body
+            const data = await Stores.find({ userId: userId })
+            res.json({ success: true, message: 'get store successfully', data: data })
+        } catch (error) {
+            res.json({ success: false, message: `Error get store controller: ${error}` })
         }
     }
 }
