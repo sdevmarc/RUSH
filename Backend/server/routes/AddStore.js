98062a4b0e9f1a7f449d94e071d363881f2d72ff
sdevmarc <suanieeee@yahoo.com>
2024-03-31 01:57:41 +0800

    Add GET endpoint for retrieving store information

--- Backend/server/routes/AddStore.js
+++ Backend/server/routes/AddStore.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const Stores = require('../controllers/Stores')
 
 router.post('/addstore', Stores.CreateStore)
+router.get('/getstore', Stores.GetStore)
 
 module.exports = router
