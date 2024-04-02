bcaf81de07df951c6d15fd19fe15ee42a8fbe6f1
sdevmarc <suanieeee@yahoo.com>
2024-04-02 20:42:37 +0800

    Remove AddStore route

--- Backend/server/routes/AddStore.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const Stores = require('../controllers/Stores')
-
-router.post('/addstore', Stores.CreateStore)
-router.get('/getstore', Stores.GetStore)
-
-module.exports = router
