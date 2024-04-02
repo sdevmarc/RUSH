5856ef2ad17b0de8c833a11df3a6c79818cb1191
sdevmarc <suanieeee@yahoo.com>
2024-04-02 20:41:49 +0800

    Remove AuthGoogle middleware

--- Backend/server/middleware/AuthGoogle.js
+++
@@ -1,9 +0,0 @@
-const AuthGoogle = async (req, res, next) => {
-    if (req.oidc.isAuthenticated()) {
-        next();
-    } else {
-        res.json({ success: false, message: 'Unauthorized' });
-    }
-}
-
-module.exports = AuthGoogle
