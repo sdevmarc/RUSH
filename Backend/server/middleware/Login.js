06ce633d59e16cd5284f986e115da38d6e7d426c
sdevmarc <suanieeee@yahoo.com>
2024-04-02 20:42:30 +0800

    Add JWT authentication to Login middleware

--- Backend/server/middleware/Login.js
+++ Backend/server/middleware/Login.js
@@ -1,17 +1,20 @@
 const bcrypt = require('bcrypt')
 const Users = require('../models/Users')
+const jwt = require('jsonwebtoken')
+require('dotenv')
 
 const LoginAuth = async (req, res, next) => {
     try {
         const { username, password } = req.body
 
         const User = await Users.findOne({ username: username })
-        
+
         if (User) {
             const isMatch = await bcrypt.compare(password, User.password);
 
             if (isMatch) {
-                res.json({ success: true, message: 'Login successful!' })
+                const token = jwt.sign({ userPass: User.password, userId: User._id }, process.env.SECRET_TOKEN, { expiresIn: '1d' })
+                res.json({ success: true, message: 'Login successful!', token: token, userId: User._id })
                 next()
             } else {
                 res.json({ success: false, message: 'Wrong Password!' });
