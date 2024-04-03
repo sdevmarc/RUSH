d9a78498b0fe217ee3656686fcbf0432add8e10e
Marc Edison <suanieeee@yahoo.com>
2024-04-03 15:12:05 +0800

    still error exist

---
+++ Frontend/Mobile/reclient/app/components/AddressModal.jsx
@@ -0,0 +1,99 @@
+// AddressModal.js
+import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, Platform, Dimensions, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { Picker } from '@react-native-picker/picker';
+import Context from './Context';
+
+const { width, height } = Dimensions.get('window');
+
+const AddressModal = ({ place, title, address, setAddressName }) => {
+    const [IsPickupAddress, setIsPickupAddress] = useState('');
+    const { IsModalOpen, setIsModalOpen } = useContext(Context);
+    const [IsAddress, setIsAddress] = useState([]);
+    const [IsMunicipalityCode, setIsMunicipalityCode] = useState()
+
+    useEffect(() => {
+        fetchMunicipality()
+    }, []);
+
+    const fetchMunicipality = async () => {
+        const response = await axios.get(`https://psgc.gitlab.io/api/provinces/025000000/${address}`);
+        const sortedData = response.data.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        })
+        setIsAddress(sortedData);
+        // setIsMunicipalityCode(data.data.municipalityCode)s
+    }
+
+    const fetchBarangay = () => {
+        if (IsPickupAddress === IsMunicipalityCode) {
+
+        }
+    }
+
+    const handleValueChange = (itemValue) => {
+        setIsPickupAddress(itemValue);
+        setAddressName(itemValue); // Call the setAddressName function with the selected value
+    };
+
+    const handlModal = (e, value) => {
+        setIsModalOpen((prevValues) => ({
+            ...prevValues,
+            [e]: value
+        }));
+    };
+
+    return (
+        <>
+            {
+                IsModalOpen[place] && Platform.OS === 'ios' && (
+                    <>
+                        <View style={{ position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', opacity: .4, zIndex: 1 }} />
+                        <View style={{ position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', zIndex: 1 }}>
+                            <View style={{ width: '100%', gap: height * 0.01, paddingHorizontal: width * 0.03, justifyContent: 'center' }}>
+                                <Text style={{ color: 'white' }}>{title}</Text>
+                                <Picker
+                                    selectedValue={IsPickupAddress}
+                                    onValueChange={handleValueChange}
+                                    mode='drop'
+                                    dropdownIconColor={'#000'}
+                                    dropdownIconRippleColor={'#d9d8d7'}
+                                    prompt='Select Business Type'
+                                    selectionColor={'#000'}
+                                    style={{ backgroundColor: '#d9d8d7', color: '#000', borderRadius: height * 0.01 }}
+                                >
+                                    {Platform.OS === 'android' && <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="" value="" />}
+                                    {IsAddress.map((item) => (
+                                        <Picker.Item key={item.code} color={Platform.OS === 'android' ? '#000' : '#000'} label={item.name} value={item.name} />
+                                    ))}
+                                </Picker>
+                                <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'center', gap: width * 0.05 }}>
+                                    <TouchableOpacity
+                                        onPress={() => handlModal(place, false)}
+                                        style={{ width: width * 0.4, height: height * 0.06, backgroundColor: '#cf2935', justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}
+                                    >
+                                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: height * 0.02 }}>Cancel</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        style={{ width: width * 0.4, height: height * 0.06, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}
+                                    >
+                                        <Text style={{ color: '#111', fontWeight: 'bold', fontSize: height * 0.02 }}>Yes</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    </>
+                )
+            }
+        </>
+    );
+};
+
+export default AddressModal;
--- Frontend/Mobile/reclient/app/screens/Seller/AddStore/ShopInformation.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/AddStore/ShopInformation.jsx
@@ -8,34 +8,59 @@ import {
     Platform,
     TouchableOpacity
 } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from 'axios'
 import Navbar from '../../../components/Navbar'
 import { useNavigation } from '@react-navigation/native'
 import BottomBar from '../../../components/BottomBar'
 import { Picker } from '@react-native-picker/picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import AddressModal from '../../../components/AddressModal'
+import Context from '../../../components/Context'
 
 const { width, height } = Dimensions.get('window')
 
 const ShopInformation = () => {
     const [IsPickupAddress, setIsPickupAddress] = useState('');
-    const [IsModalMunicipality, setIsModalMunicipality] = useState(false)
-    const [IsModalBarangay, setIsModalBarangay] = useState(false)
+    const [IsModalOpen, setIsModalOpen] = useState({
+        municipality: false,
+        barangay: false
+    })
     const [values, setValues] = useState({
         shopName: '',
         pickupAddress: '',
         email: '',
         mobileNumber: ''
     })
+    const [addressName, setAddressName] = useState({
+        municipality: '',
+        barangay: ''
+    })
+    const [IsAddress, setIsAddress] = useState([])
 
     const navigation = useNavigation()
 
     useEffect(() => {
-
+        fetchAddress()
     }, [])
 
-    const handleModalMunicipality = () => {
-        setIsModalMunicipality(true)
+    const fetchAddress = async () => {
+        const data = await axios.get('https://psgc.gitlab.io/api/provinces/025000000/municipalities')
+
+    }
+
+    const handleUpdateMunicipality = useCallback((value) => {
+        setAddressName(prevState => ({
+            ...prevState,
+            municipality: value
+        }));
+    }, [setAddressName]);
+
+    const handleModal = (e, value) => {
+        setIsModalOpen((prevValue) => ({
+            ...prevValue,
+            [e]: value
+        }))
     }
 
     const handleOnChangeShopName = (value) => {
@@ -68,60 +93,20 @@ const ShopInformation = () => {
             <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
             <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
                 <Navbar title='Shop Information' backgroundColor='#323d48' />
-                {
-                    IsModalMunicipality === true && Platform.OS === 'ios' &&
-                    (
-                        <>
-                            <View style={{ position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', opacity: .4, zIndex: 1 }} />
-                            <View
-                                style={{ position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', zIndex: 1 }}
-                            >
-
-                                <View style={{ width: '100%', gap: height * 0.01, paddingHorizontal: width * 0.03, justifyContent: 'center' }}>
-                                    <Text style={{ color: 'white', fontWeight: 'bold' }}>
-                                        City
-                                    </Text>
-                                    <Picker
-                                        selectedValue={IsPickupAddress}
-                                        onValueChange={(itemValue, itemIndex) => {
-                                            setIsPickupAddress(itemValue)
-                                        }}
-                                        mode='drop'
-                                        dropdownIconColor={'#000'}
-                                        dropdownIconRippleColor={'#d9d8d7'}
-                                        prompt='Select Business Type'
-                                        selectionColor={'#000'}
-                                        style={{ backgroundColor: '#d9d8d7', color: '#000', borderRadius: height * 0.01 }}
-                                    >
-                                        {
-                                            Platform.OS === 'android'
-                                                ? null
-                                                : <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="" value="" />
-                                        }
-
-                                        <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="Individual" value="Individual" />
-                                        <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="Business" value="Business" />
-                                    </Picker>
-                                </View>
-                                <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
-
-                                </View>
-                            </View>
-                        </>
-                    )
-
-                }
-
+                <Context.Provider value={{ IsModalOpen, setIsModalOpen }}>
+                    <AddressModal place='municipality' title='municipality' address='municipalities' setAddressName={handleUpdateMunicipality} />
+                    <AddressModal place='barangay' title='barangay' address='barangays' />
+                </Context.Provider>
                 <ScrollView>
                     <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
                         <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01 }}>
                             <View style={{ width: '100%', gap: height * 0.01 }}>
-                                <Text style={{ color: 'white', fontWeight: 'bold' }}>
-                                    Shop Name
-                                </Text>
+                                <Text style={{ color: 'white', fontWeight: 'bold' }}>Shop Name</Text>
                                 <TextInput
                                     onChangeText={handleOnChangeShopName}
-                                    style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }} placeholder='What is your shop name?' />
+                                    style={{ height: height * 0.06, backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: width * 0.05, fontSize: width * 0.035 }}
+                                    placeholder='What is your shop name?'
+                                />
                             </View>
 
                             <View style={{ width: '100%', flexDirection: 'row' }}>
@@ -129,31 +114,94 @@ const ShopInformation = () => {
                                     Pickup Address
                                 </Text>
                             </View>
-                            <TouchableOpacity
-                                onPress={handleModalMunicipality}
-                                style={{ width: '100%', gap: height * 0.003, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01, justifyContent: 'space-between' }}
-                            >
-                                <View style={{ width: '100%', gap: height * 0.005, flexDirection: 'row', paddingHorizontal: width * 0.02, paddingVertical: height * 0.01, justifyContent: 'space-between' }}>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '600' }}>
-                                        Municipality
-                                    </Text>
-                                    <Text style={{ width: '50%', color: 'white', textAlign: 'right' }} numberOfLines={1} ellipsizeMode='tail'>
-                                        Marc Edison
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={{ width: '100%', gap: height * 0.003, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01, justifyContent: 'space-between' }}
-                            >
-                                <View style={{ width: '100%', gap: height * 0.005, flexDirection: 'row', paddingHorizontal: width * 0.02, paddingVertical: height * 0.01, justifyContent: 'space-between' }}>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '600' }}>
-                                        Barangay
-                                    </Text>
-                                    <Text style={{ width: '50%', color: 'white', textAlign: 'right' }} numberOfLines={1} ellipsizeMode='tail'>
-                                        Marc Edison
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
+                            {Platform.OS === 'ios'
+                                ? (
+                                    <>
+                                        <TouchableOpacity
+                                            onPress={() => handleModal('municipality', true)}
+                                            style={{ width: '100%', gap: height * 0.003, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01, justifyContent: 'space-between' }}
+                                        >
+                                            <View style={{ width: '100%', gap: height * 0.005, flexDirection: 'row', paddingHorizontal: width * 0.02, paddingVertical: height * 0.01, justifyContent: 'space-between' }}>
+                                                <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '600' }}>
+                                                    Municipality
+                                                </Text>
+                                                <Text style={{ width: '50%', color: 'white', textAlign: 'right' }} numberOfLines={1} ellipsizeMode='tail'>
+                                                    {addressName.municipality}
+                                                </Text>
+                                            </View>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity
+                                            onPress={() => handleModal('barangay', true)}
+                                            style={{ width: '100%', gap: height * 0.003, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01, justifyContent: 'space-between' }}
+                                        >
+                                            <View style={{ width: '100%', gap: height * 0.005, flexDirection: 'row', paddingHorizontal: width * 0.02, paddingVertical: height * 0.01, justifyContent: 'space-between' }}>
+                                                <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '600' }}>
+                                                    Barangay
+                                                </Text>
+                                                <Text style={{ width: '50%', color: 'white', textAlign: 'right' }} numberOfLines={1} ellipsizeMode='tail'>
+                                                    Marc Edison
+                                                </Text>
+                                            </View>
+                                        </TouchableOpacity>
+                                    </>
+                                )
+                                :
+                                (
+                                    <>
+                                        <View style={{ width: '100%', paddingHorizontal: width * 0.03, gap: height * 0.01 }}>
+                                            <View style={{ width: '100%', gap: height * 0.01 }}>
+                                                <Text style={{ width: '50%', color: 'white', fontWeight: '500' }}>
+                                                    Municipality
+                                                </Text>
+                                                <Picker
+                                                    selectedValue={IsPickupAddress}
+                                                    onValueChange={(itemValue, itemIndex) => {
+                                                        setIsPickupAddress(itemValue)
+                                                    }}
+                                                    mode='drop'
+                                                    dropdownIconColor={'#000'}
+                                                    dropdownIconRippleColor={'#d9d8d7'}
+                                                    prompt='Select Muhn'
+                                                    selectionColor={'#000'}
+                                                    style={{ backgroundColor: '#d9d8d7', color: '#000', borderRadius: height * 0.01 }}
+                                                >
+                                                    {
+                                                        Platform.OS === 'ios' && <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="" value="" />
+                                                    }
+                                                    <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="Individual" value="Individual" />
+                                                    <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="Business" value="Business" />
+                                                </Picker>
+                                            </View>
+
+                                            <View style={{ width: '100%', gap: height * 0.01 }}>
+                                                <Text style={{ width: '50%', color: 'white', fontWeight: '500' }}>
+                                                    Barangay
+                                                </Text>
+                                                <Picker
+                                                    selectedValue={IsPickupAddress}
+                                                    onValueChange={(itemValue, itemIndex) => {
+                                                        setIsPickupAddress(itemValue)
+                                                    }}
+                                                    mode='drop'
+                                                    dropdownIconColor={'#000'}
+                                                    dropdownIconRippleColor={'#d9d8d7'}
+                                                    prompt='Select Barangay'
+                                                    selectionColor={'#000'}
+                                                    style={{ backgroundColor: '#d9d8d7', color: '#000', borderRadius: height * 0.01 }}
+                                                >
+                                                    {
+                                                        Platform.OS === 'ios' && <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="" value="" />
+                                                    }
+                                                    <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="Individual" value="Individual" />
+                                                    <Picker.Item color={Platform.OS === 'android' ? '#000' : '#000'} label="Business" value="Business" />
+                                                </Picker>
+                                            </View>
+                                        </View>
+
+                                    </>
+                                )
+                            }
+
 
                             <TouchableOpacity style={{ width: '100%', gap: height * 0.01 }}>
                                 <Text style={{ color: 'white', fontWeight: 'bold' }}>
