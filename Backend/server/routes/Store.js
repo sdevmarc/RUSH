c226a35d4b054261a497f8545cf6ea19e5f9790d
sdevmarc <suanieeee@yahoo.com>
2024-04-04 22:28:03 +0800

    Add checkStoreIfExists middleware and UpdateSellerType function to addstore route

--- Backend/server/routes/Store.js
+++ Backend/server/routes/Store.js
@@ -3,8 +3,9 @@ const router = express.Router()
 
 const Stores = require('../controllers/Stores')
 const authenticateUser = require('../middleware/AuthHome')
+const checkStoreIfExists = require('../middleware/checkStore')
 
-router.post('/addstore', authenticateUser, Stores.CreateStore)
+router.post('/addstore', authenticateUser, checkStoreIfExists, Stores.UpdateSellerType, Stores.CreateStore)
 router.get('/getstore', authenticateUser, Stores.GetStore)
 
 module.exports = router
