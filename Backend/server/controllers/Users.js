1a3a7876d0dd0feb15ca8cc76be2d8db20abc576
sdevmarc <suanieeee@yahoo.com>
2024-04-04 22:26:36 +0800

    Update error messages in user controller

--- Backend/server/controllers/Users.js
+++ Backend/server/controllers/Users.js
@@ -4,10 +4,22 @@ const UserController = {
     CreateUser: async (req, res) => {
         try {
             const values = req.body
+
             await Users.create(values)
             res.json({ success: true, message: 'User added successfully!' })
         } catch (error) {
-            res.json({ success: false, message: `Error user controller: ${error}` })
+            res.json({ success: false, message: `Error adding user controller: ${error}` })
+        }
+    },
+    FetchUser: async (req, res) => {
+        try {
+            const { userId } = req.params
+
+            const data = await Users.findOne({ _id: userId })
+            const { name, username, contactno, UserType } = data
+            res.json({ success: true, message: 'Fetching user successful', data: { username, name, contactno, UserType } })
+        } catch (error) {
+            res.json({ success: false, message: `Error getting user controller: ${error}` })
         }
     }
 }
