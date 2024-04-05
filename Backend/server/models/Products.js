e3dfd46ddc84280d8a9bef46c9786f1d65dfe8f0
sdevmarc <suanieeee@yahoo.com>
2024-04-06 00:46:44 +0800

    Add Products model for storing product information

---
+++ Backend/server/models/Products.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    storeId: {
+        type: String,
+        required: true
+    },
+    productInformation: {
+        productName: String,
+        productDescription: String,
+        category: String,
+        date: String,
+        gallery: [
+            { uri: String }
+        ],
+        sizes: [
+            { size: String }
+        ],
+        shippingAvailability: [
+            { shippingName: String }
+        ],
+        price: {
+            type: String,
+            required: true
+        },
+        shippingFee: {
+            type: String,
+            required: true
+        }
+    }
+}, { timestamps: true })
+
+const ProductModel = mongoose.model('Products', ProductSchema)
+module.exports = ProductModel
