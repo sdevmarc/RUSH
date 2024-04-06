5ba49057c88062c8ac63970ea886f3cc2d92aa53
sdevmarc <suanieeee@yahoo.com>
2024-04-07 00:18:09 +0800

    Refactor Product screen in Mobile app to add navigation and UI improvements

--- Frontend/Mobile/reclient/app/screens/Seller/Product.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/Product.jsx
@@ -1,11 +1,72 @@
-import { View, Text } from 'react-native'
+import {
+    View,
+    Text,
+    Dimensions,
+    StatusBar,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import Navbar from '../../components/Navbar'
+import { MaterialIcons } from '@expo/vector-icons';
+
+const { width, height } = Dimensions.get('window')
 
 const Product = () => {
+
+    const navigation = useNavigation()
+
+    const handleOnPressAddProduct = () => {
+        navigation.navigate('AddProduct')
+    }
+
     return (
         <>
-            <Navbar title='Products' backgroundColor='#323d48' />
+            <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+            <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
+                <Navbar title='Products' backgroundColor='#323d48' />
+                <ScrollView>
+                    <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
+                        <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01 }}>
+                            <TouchableOpacity
+
+                                style={{
+                                    padding: width * 0.03,
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    paddingHorizontal: width * 0.03,
+                                    backgroundColor: '#4a4c59',
+                                    borderRadius: height * 0.01
+                                }}
+                            >
+                                <Text style={{ color: '#fff', fontWeight: 'bold' }}>
+                                    View Products
+                                </Text>
+                                <MaterialIcons name="arrow-forward-ios" size={width * 0.05} color="white" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={handleOnPressAddProduct}
+                                style={{
+                                    padding: width * 0.03,
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    paddingHorizontal: width * 0.03,
+                                    backgroundColor: '#4a4c59',
+                                    borderRadius: height * 0.01
+                                }}
+                            >
+                                <Text style={{ color: '#fff', fontWeight: 'bold' }}>
+                                    Add Product
+                                </Text>
+                                <MaterialIcons name="arrow-forward-ios" size={width * 0.05} color="white" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView >
+            </View >
         </>
     )
 }
