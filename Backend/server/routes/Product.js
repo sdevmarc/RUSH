2a1078b1f34680d3945eb36a13a76063cfd8f849
Marc Edison <suanieeee@yahoo.com>
2024-04-09 02:53:25 +0800

    Add new API endpoints for fetching and selecting products in Product.js

--- Backend/server/routes/Product.js
+++ Backend/server/routes/Product.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const Products = require('../controllers/Products')
 
 router.post('/addproduct', Products.AddProduct)
+router.get('/getproducts/:storeId', Products.FetchProducts)
+router.get('/selectproduct/:id', Products.SelectedItem)
 
 module.exports = router
