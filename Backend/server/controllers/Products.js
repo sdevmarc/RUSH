eb59029f0702f3dcefa58a03d8d22cdaacc533ee
Marc Edison <suanieeee@yahoo.com>
2024-04-09 02:53:15 +0800

    Refactor Products.js to add new API endpoints for fetching and selecting products

--- Backend/server/controllers/Products.js
+++ Backend/server/controllers/Products.js
@@ -6,17 +6,41 @@ const ProductController = {
             const values = req.body
 
             const data = await Product.create(values)
-            if(data) {
-                console.log('Values added successfull')
+            if (data) {
                 res.json({ success: true, message: 'Product successfully added' })
             } else {
-                console.log('Values added not successfull')
                 res.json({ success: false, message: 'Product successfully not added' })
             }
-            // res.json({ success: true, message: 'Product successfully added' })
-            
         } catch (error) {
             res.json({ success: false, message: `Error adding product controller: ${error}`, error: error })
+        }
+    },
+    FetchProducts: async (req, res) => {
+        try {
+            const { storeId } = req.params
+            const data = await Product.find({ storeId: storeId })
+            if (data) {
+                res.json({ success: true, message: 'Product successfully fetched', data })
+            } else {
+                res.json({ success: false, message: 'Product successfully not fetched' })
+            }
+        } catch (error) {
+            res.json({ success: false, message: `Error fetching product controller: ${error}`, error: error })
+        }
+    },
+    SelectedItem: async (req, res) => {
+        try {
+            const { id } = req.params
+            const data = await Product.findById({ _id: id })
+
+            if (res) {
+                res.json({ success: false, message: 'Product did  fetched', data })
+            } else {
+                res.json({ success: false, message: 'Product did not fetched' })
+            }
+            
+        } catch (error) {
+            res.json({ success: false, message: `Error fetching the selected product controller: ${error}`, error: error })
         }
     }
 }
