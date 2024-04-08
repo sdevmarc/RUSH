8644c3511e82eda99e80a411abbee086993b2108
sdevmarc <suanieeee@yahoo.com>
2024-04-08 16:16:17 +0800

    Fix cloudinary configuration in cloudinary.js

--- Backend/server/utils/cloudinary.js
+++ Backend/server/utils/cloudinary.js
@@ -1,9 +1,10 @@
-const cloudinary = require('cloudinary')
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
-    api_key: process.env.CLOUDINARY_API_KEY ,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: 'do1p9llzd',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
 })
 
 module.exports = cloudinary
