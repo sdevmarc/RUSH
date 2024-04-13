a362abb9b375b3d77d8144b2d6b50dd283c86a8f
sdevmarc <suanieeee@yahoo.com>
2024-04-13 16:26:06 +0800

    Refactor Account.jsx, Store.jsx, SelectedStore component, Drawer.jsx, GetStore function, AddProducts component, Store.jsx, Product.jsx, and Home.jsx to improve code readability and handle navigation, AsyncStorage, image upload, store selection, and drawer navigation in Mobile app. Add new API endpoints for fetching and selecting products in Product.js. Add jwt dependency to Stores.js controller.

--- Frontend/Mobile/reclient/app/screens/Settings/Orders.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/Orders.jsx
@@ -4,11 +4,13 @@ import {
     StatusBar,
     ScrollView,
     Dimensions,
-    TouchableOpacity
+    TouchableOpacity,
+    Image
 } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
+import * as Color from '../../../utils/colors'
 
 const { width, height } = Dimensions.get('window')
 
@@ -18,50 +20,88 @@ const Orders = () => {
     return (
         <>
             <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
-            <View
-                style={{
-                    position: 'absolute',
-                    width: '100%',
-                    height: height * 0.12,
-                    zIndex: 1,
-                    backgroundColor: '#323d48',
-                    paddingVertical: height * 0.009
-                }}
-            >
+            <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
                 <View
                     style={{
+                        position: 'absolute',
                         width: '100%',
-                        height: '100%',
-                        paddingHorizontal: width * 0.05,
-                        width: width,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'flex-end',
-                        zIndex: 1
+                        height: height * 0.12,
+                        zIndex: 1,
+                        backgroundColor: '#323d48',
+                        paddingVertical: height * 0.009
                     }}
                 >
-                    <TouchableOpacity
-                        onPress={() => navigation.openDrawer()}
+                    <View
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            paddingHorizontal: width * 0.05,
+                            width: width,
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'flex-end',
+                            zIndex: 1
+                        }}
                     >
-                        <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="white" />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
-                    <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
-                        Orders
-                    </Text>
-                </View>
-            </View>
-            <ScrollView>
-                <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
-                    <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01 }}>
-                        <Text>
-                            Orders Text
+                        <TouchableOpacity
+                            onPress={() => navigation.openDrawer()}
+                        >
+                            <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="white" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
+                            Orders
                         </Text>
                     </View>
                 </View>
-            </ScrollView>
-
+                <ScrollView>
+                    <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
+                        <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01, paddingBottom: height * 0.2 }}>
+                            <TouchableOpacity style={{ width: '100%', height: height * 0.23, backgroundColor: Color.secondaryColor, borderRadius: height * 0.01, overflow: 'hidden' }}>
+                                <View style={{ width: '100%', height: '75%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.02 }}>
+                                    <View style={{ width: '40%', height: '90%', overflow: 'hidden', backgroundColor: 'black', borderRadius: height * 0.01 }}>
+                                        <Image source={{ uri: 'source.unsplash.com/a-man-with-pink-hair-looking-up-at-the-sky-kY_0Xv0_rNs' }} style={{ width: '100%', height: '100%' }} resizeMode='cover' />
+                                    </View>
+                                    <View style={{ width: '60%', flexDirection: 'rwo', paddingHorizontal: width * 0.02, gap: height * 0.01 }}>
+                                        <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                            <Text style={{ color: 'white', fontWeight: '500' }}>
+                                                Product Name:
+                                            </Text>
+                                            <Text style={{ width: '100%', color: 'white' }} numberOfLines={1} ellipsizeMode='tail' >
+                                                The Product
+                                            </Text>
+                                        </View>
+                                        <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                            <Text style={{ color: 'white', fontWeight: '500' }}>
+                                                Store Name:
+                                            </Text>
+                                            <Text style={{ width: '100%', color: 'white' }} numberOfLines={1} ellipsizeMode='tail' >
+                                                The Product
+                                            </Text>
+                                        </View>
+                                        <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                            <Text style={{ color: 'white', fontWeight: '500' }}>
+                                                Total Fee:
+                                            </Text>
+                                            <Text style={{ width: '100%', color: 'white' }} numberOfLines={1} ellipsizeMode='tail' >
+                                                P 100.00
+                                            </Text>
+                                        </View>
+                                    </View>
+                                </View>
+                                <View style={{ width: '100%', height: '20%', paddingHorizontal: width * 0.02 }}>
+                                    <TouchableOpacity style={{ width: '100%', height: '100%', backgroundColor: Color.accent, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
+                                        <Text style={{ fontWeight: 'bold', color: 'white' }}>
+                                            RATE ITEM
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View >
+                </ScrollView >
+            </View >
         </>
     )
 }
