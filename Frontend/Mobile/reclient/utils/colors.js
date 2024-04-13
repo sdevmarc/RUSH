77deb4c690c770a70d40db1b48bf95be4fa73939
sdevmarc <suanieeee@yahoo.com>
2024-04-13 16:26:24 +0800

    Refactor colors.js to add primary, secondary, accent, font, white, black, and idle colors in Mobile app.

---
+++ Frontend/Mobile/reclient/utils/colors.js
@@ -0,0 +1,8 @@
+export const primaryColor = '#323d48'
+export const secondaryColor = '#4a4c59'
+export const accent = '#d7a152'
+export const fontColor = '#F8F8F8'
+export const whiteColor = '#F8F8F8'
+export const black = '#111111'
+export const idleColor = '#e6e6e6'
+
