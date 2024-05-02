a0626a6294d6426f86c818fbdf5ec5c32387f291
sdevmarc <suanieeee@yahoo.com>
2024-05-02 08:51:41 +0800

    Update Routes.jsx to use new page components

--- Frontend/Admin - Website/admin/src/Routes.jsx
+++ Frontend/Admin - Website/admin/src/Routes.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
-import Admin from './pages/Admin'
+import Dashboard from './pages/Dashboard'
+import Users from './pages/Users'
+import Renters from './pages/Renters'
+import Rentees from './pages/Rentees'
+import Shops from './pages/Shops'
+import Settings from './pages/Settings'
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
-    { path: '/admin', element: <Admin /> }
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/users', element: <Users /> },
+    { path: '/renters', element: <Renters /> },
+    { path: '/rentees', element: <Rentees /> },
+    { path: '/shops', element: <Shops /> },
+    { path: '/settings', element: <Settings /> }
 ])
 
 export default Routes
