3c1498516e983e33866aad4ab22ec076e7a9ca67
sdevmarc <suanieeee@yahoo.com>
2024-05-02 08:52:58 +0800

    Add Shops page component to admin/src/pages/Shops.jsx

---
+++ Frontend/Admin - Website/admin/src/pages/Shops.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+export default function Shops() {
+  return (
+    <>
+            <div className="w-full h-screen">
+                <Navbar />
+                <Sidebar />
+            </div>
+        </>
+  )
+}
