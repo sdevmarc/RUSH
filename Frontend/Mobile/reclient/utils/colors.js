de694ae8e4047428a4bf08ea8ec5c2709d546b0e
Marc Edison <suanieeee@yahoo.com>
2024-05-05 03:06:24 +0800

    refactor: Update black color code to #000000 and add semiblack color code
    - Update the black color code in the colors.js file to #000000 for consistency
    - Add a new semiblack color code in the colors.js file for future use

--- Frontend/Mobile/reclient/utils/colors.js
+++ Frontend/Mobile/reclient/utils/colors.js
@@ -1,7 +1,8 @@
 export const backgroundColor = '#ffffff'
 export const fontColor = '#000'
 export const whiteColor = '#ffffff'
-export const black = '#000'
+export const black = '#000000'
+export const semiblack = '#333333'
 export const idleColor = '#EFEFEF'
 export const orange = '#E1793C'
 
