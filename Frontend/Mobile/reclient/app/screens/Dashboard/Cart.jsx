d17460fb3d93a227b13cb79c87eac31465037943
sdevmarc <suanieeee@yahoo.com>
2024-05-04 22:40:49 +0800

    chore: Update Product screen styling and add search functionality

--- Frontend/Mobile/reclient/app/screens/Dashboard/Cart.jsx
+++ Frontend/Mobile/reclient/app/screens/Dashboard/Cart.jsx
@@ -3,28 +3,28 @@ import {
     Dimensions,
     Text,
     StatusBar,
-    Animated,
     TouchableOpacity,
     ScrollView,
     Image,
-    Button
 } from 'react-native'
-import React, { useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { Ionicons } from '@expo/vector-icons';
-import Navbar from '../../components/Navbar';
+import Navbar from '../../components/Navbar'
+import * as Colors from '../../../utils/colors'
+import BottomBar from '../../components/BottomBar'
+import axios from 'axios'
+
 
 const { width, height } = Dimensions.get('window')
 
-const Cart = () => {
+const Cart = ({ route }) => {
+    const [shopName, setShopName] = useState('')
+    const [values, setValues] = useState([])
     const navigation = useNavigation()
-    const scrollY = useRef(new Animated.Value(0)).current;
 
-    const headerHeight = scrollY.interpolate({
-        inputRange: [0, height * 0.2],
-        outputRange: [height * 0.13, height * 0.11],
-        extrapolate: 'clamp',
-    })
+    useEffect(() => {
+        fetchProductItem()
+    }, [])
 
     const handleCheckout = () => {
         navigation.reset({
@@ -32,6 +32,13 @@ const Cart = () => {
             routes: [{ name: 'SuccessfulCheckout' }]
         })
         // navigation.replace('SuccessfulCheckout')
+    }
+
+    const fetchProductItem = async () => {
+        const { id, shopName } = route.params
+        const res = await axios.get(`http://${address}/api/selectproduct/${id}`)
+        setValues(res.data.data)
+        setShopName(shopName)
     }
 
     const handleAddress = () => {
@@ -48,32 +55,32 @@ const Cart = () => {
 
     return (
         <>
-            <StatusBar barStyle="light-content" />
-            <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
-                <Navbar title='Checkout' backgroundColor='#323d48' />
+            <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+            <View style={{ width: width, height: height, backgroundColor: Colors.backgroundColor }}>
+                <Navbar title='Checkout' backgroundColor={Colors.backgroundColor} tintColor={Colors.fontColor} />
                 <ScrollView>
                     <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
                         <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.02 }}>
                             <TouchableOpacity
                                 onPress={handleAddress}
-                                style={{ width: '100%', gap: height * 0.007, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01 }}
+                                style={{ width: '100%', gap: height * 0.007, backgroundColor: Colors.idleColor, padding: width * 0.03, borderRadius: height * 0.01 }}
                             >
                                 <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                         Delivery Address
                                     </Text>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '500' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: '500' }}>
                                         Edit
                                     </Text>
                                 </View>
                                 <View style={{ width: '100%', paddingHorizontal: width * 0.03, gap: height * 0.007 }}>
-                                    <Text style={{ color: 'white', textAlign: 'justify' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify' }}>
                                         MARC EDISON D. SUAREZ
                                     </Text>
-                                    <Text style={{ color: 'white', textAlign: 'justify' }} numberOfLines={2} ellipsizeMode='tail'>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify' }} numberOfLines={2} ellipsizeMode='tail'>
                                         Address ni marc, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum perspiciatis aut maxime autem illum sed fugit? Doloribus harum voluptas vitae magni ad ab dicta! Nisi impedit voluptatibus expedita exercitationem similique.
                                     </Text>
-                                    <Text style={{ color: 'white', textAlign: 'justify' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify' }}>
                                         +63 933335555
                                     </Text>
                                 </View>
@@ -87,72 +94,72 @@ const Cart = () => {
                                     padding: height * 0.01,
                                     alignItems: 'center',
                                     flexDirection: 'row',
-                                    backgroundColor: '#4a4c59'
+                                    backgroundColor: Colors.idleColor
                                 }}
                             >
-                                <View style={{ overflow: 'hidden', width: '40%', height: '100%', backgroundColor: 'white', borderRadius: height * 0.01 }}>
+                                <View style={{ overflow: 'hidden', width: '40%', height: '100%', backgroundColor: Colors.fontColor, borderRadius: height * 0.01 }}>
                                     <Image
-                                        source={{ uri: 'https://source.unsplash.com/white-v-neck-shirt-on-brown-clothes-hanger-p8Drpg_duLw' }}
+                                        source={{ uri: values?.productInformation?.gallery[0]?.uri }}
                                         resizeMode='cover'
                                         style={{ width: '100%', height: '100%' }}
                                     />
                                 </View>
                                 <View style={{ width: '60%', height: '100%', padding: height * 0.01, justifyContent: 'center', gap: height * 0.01 }}>
                                     <View>
-                                        <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                             Product Name
                                         </Text>
-                                        <Text style={{ color: 'white', textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                            Sando
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                            {values?.productInformation?.productName}
                                         </Text>
                                     </View>
                                     <View>
-                                        <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                             Store Name
                                         </Text>
-                                        <Text style={{ color: 'white', textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                            Barassi
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                            {shopName}
                                         </Text>
                                     </View>
                                     <View>
-                                        <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                             Price
                                         </Text>
-                                        <Text style={{ color: 'white', textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                            ₱ 58
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                            ₱ {values?.productInformation?.price}.00
                                         </Text>
                                     </View>
                                 </View>
                             </View>
                             <TouchableOpacity
                                 onPress={handleShippingOption}
-                                style={{ width: '100%', gap: height * 0.007, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01 }}
+                                style={{ width: '100%', gap: height * 0.007, backgroundColor: Colors.idleColor, padding: width * 0.03, borderRadius: height * 0.01 }}
                             >
                                 <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                         Shipping Option
                                     </Text>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '500' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: '500' }}>
                                         Edit
                                     </Text>
                                 </View>
                                 <View style={{ width: '100%', paddingHorizontal: width * 0.03, gap: height * 0.007 }}>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '600' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: '600' }}>
                                         Delivery
                                     </Text>
-                                    <Text style={{ color: 'white', textAlign: 'justify' }}>
+                                    <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontSize: width * 0.03 }}>
                                         Make sure your delivery address is set to your correct location.
                                     </Text>
                                 </View>
                             </TouchableOpacity>
                             <TouchableOpacity
                                 onPress={handlePaymentOption}
-                                style={{ width: '100%', gap: height * 0.007, backgroundColor: '#4a4c59', padding: width * 0.03, borderRadius: height * 0.01, justifyContent: 'space-between', flexDirection: 'row' }}
+                                style={{ width: '100%', gap: height * 0.007, backgroundColor: Colors.idleColor, padding: width * 0.03, borderRadius: height * 0.01, justifyContent: 'space-between', flexDirection: 'row' }}
                             >
-                                <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>
+                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                     Payment Method
                                 </Text>
-                                <Text style={{ color: 'white', textAlign: 'justify' }}>
+                                <Text style={{ color: Colors.fontColor, textAlign: 'justify' }}>
                                     Choose payment method
                                 </Text>
 
@@ -160,71 +167,30 @@ const Cart = () => {
                             <View
                                 style={{ width: '100%', justifyContent: 'center', gap: height * 0.01 }}
                             >
-                                <Text style={{ color: 'white', textAlign: 'justify', fontWeight: 'bold' }}>Payment Details</Text>
+                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>Payment Details</Text>
                                 <View style={{ width: '100%', gap: height * 0.01 }}>
                                     <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                        <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '500' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: '500' }}>
                                             Merchandise Subtotal
                                         </Text>
-                                        <Text style={{ color: 'white', textAlign: 'justify' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify' }}>
                                             P18.00
                                         </Text>
                                     </View>
                                     <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                        <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '500' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: '500' }}>
                                             Shipping Subtotal
                                         </Text>
-                                        <Text style={{ color: 'white', textAlign: 'justify' }}>
+                                        <Text style={{ color: Colors.fontColor, textAlign: 'justify' }}>
                                             P58.00
                                         </Text>
                                     </View>
-                                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                        <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '500' }}>
-                                            Merchandise Subtotal
-                                        </Text>
-                                        <Text style={{ color: 'white', textAlign: 'justify' }}>
-                                            P18.00
-                                        </Text>
-                                    </View>
                                 </View>
-                            </View>
-                            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: width * 0.01, alignItems: 'center' }}>
-                                <View
-                                    style={{
-                                        width: width * 0.35,
-                                        height: height * 0.06,
-                                        justifyContent: 'center',
-                                        alignItems: 'flex-start'
-                                    }}
-                                >
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontWeight: '500', fontSize: width * 0.04 }}>
-                                        Total Payment
-                                    </Text>
-                                    <Text style={{ color: 'white', textAlign: 'justify', fontSize: width * 0.05, fontWeight: 'bold' }}>
-                                        ₱ 94.00
-                                    </Text>
-                                </View>
-
-                                <TouchableOpacity
-                                    onPress={handleCheckout}
-                                    style={{
-                                        width: width * 0.55,
-                                        height: height * 0.06,
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        backgroundColor: '#d7a152',
-                                        borderRadius: height * 0.02
-                                    }}
-                                >
-                                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: width * 0.04 }}>
-                                        Checkout
-                                    </Text>
-                                </TouchableOpacity>
                             </View>
                         </View>
-
                     </View >
                 </ScrollView >
+                <BottomBar subtitle={`Total Payment`} suboutput={`₱ 94.00`} redirect={handleCheckout} title={`Checkout`} />
             </View >
         </>
     )
