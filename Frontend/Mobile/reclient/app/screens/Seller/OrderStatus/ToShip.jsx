cfe3d740cdaad6719c25b3db66b65cc2447f0e73
Marc Edison <suanieeee@yahoo.com>
2024-05-08 00:51:41 +0800

    feat: Add Unreturned, Reviews, and Cancelled screens for order status updates
    This commit adds the Unreturned, Reviews, and Cancelled screens to the Seller/OrderStatus directory. These screens provide additional functionality for managing order status updates, including the ability to view unreturned orders, customer reviews, and cancelled orders. This feature enhances the user experience by providing a comprehensive set of screens for handling different aspects of order management.

--- Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/ToShip.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/ToShip.jsx
@@ -5,17 +5,34 @@ import {
     Dimensions,
     ScrollView,
     TouchableOpacity,
-    Image
+    Image,
+    Alert
 } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import * as Colors from '../../../../utils/colors'
 import Navbar from '../../../components/Navbar'
+import axios from 'axios'
+import address from '../../../../config/host'
 
 const { width, height } = Dimensions.get('window')
 
 export default function ToShip() {
     const navigation = useNavigation()
+    const [values, setValues] = useState([])
+
+    useEffect(() => {
+        fetchPending()
+    }, [])
+
+    const fetchPending = async () => {
+        const res = await axios.get(`http:${address}/api/viewtransactions`)
+        if (res?.data?.success) {
+            setValues(res?.data?.data)
+        } else {
+            Alert.alert(res?.data?.message)
+        }
+    }
 
     const handleViewOrder = () => {
         navigation.navigate('Summary')
@@ -32,64 +49,69 @@ export default function ToShip() {
                             <Text style={{ fontWeight: '600', fontSize: height * 0.017 }}>
                                 Pending Orders
                             </Text>
-                            <View style={{ width: '100%', backgroundColor: Colors.idleColor, borderRadius: height * 0.01 }}>
-                                <View style={{ overflow: 'hidden', width: '100%', height: height * 0.27, padding: height * 0.01, justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <View style={{ width: '100%', height: '70%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', }}>
-                                        <View style={{ overflow: 'hidden', width: '40%', height: '100%', backgroundColor: Colors.fontColor, borderRadius: height * 0.01 }}>
-                                            <Image
-                                                source={{ uri: 'http://source.unsplash.com/woman-standing-on-railing-w6Lb5Px8a6g' }}
-                                                resizeMode='cover'
-                                                style={{ width: '100%', height: '100%' }}
-                                            />
+                            {values?.map((item) => (
+                                <View
+                                    key={item?.transaction?._id}
+                                    style={{ width: '100%', backgroundColor: Colors.idleColor, borderRadius: height * 0.01 }}>
+                                    <View style={{ overflow: 'hidden', width: '100%', height: height * 0.27, padding: height * 0.01, justifyContent: 'space-between', alignItems: 'center' }}>
+                                        <View style={{ width: '100%', height: '70%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', }}>
+                                            <View style={{ overflow: 'hidden', width: '40%', height: '100%', backgroundColor: Colors.fontColor, borderRadius: height * 0.01 }}>
+                                                <Image
+                                                    source={{ uri: item?.product?.productInformation?.gallery[0]?.uri }}
+                                                    resizeMode='cover'
+                                                    style={{ width: '100%', height: '100%' }}
+                                                />
+                                            </View>
+                                            <View style={{ width: '60%', height: '100%', padding: height * 0.01, justifyContent: 'center', gap: height * 0.01 }}>
+                                                <View style={{ width: '100%' }}>
+                                                    <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
+                                                        Product Name
+                                                    </Text>
+                                                    <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                                        {item?.product?.productInformation?.productName}
+                                                    </Text>
+                                                </View>
+                                                <View style={{ width: '100%' }}>
+                                                    <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
+                                                        Store Name
+                                                    </Text>
+                                                    <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                                    {item?.store?.shopInformation?.shopName}
+                                                    </Text>
+                                                </View>
+                                                <View style={{ width: '100%' }}>
+                                                    <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
+                                                        Price
+                                                    </Text>
+                                                    <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                                        ₱ {item?.product?.productInformation?.price}.00
+                                                    </Text>
+                                                </View>
+                                            </View>
                                         </View>
-                                        <View style={{ width: '60%', height: '100%', padding: height * 0.01, justifyContent: 'center', gap: height * 0.01 }}>
-                                            <View style={{ width: '100%' }}>
-                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
-                                                    Product Name
-                                                </Text>
-                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                                    Titesasdasd
-                                                </Text>
-                                            </View>
-                                            <View style={{ width: '100%' }}>
-                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
-                                                    Store Name
-                                                </Text>
-                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                                    Titesasdasd
-                                                </Text>
-                                            </View>
-                                            <View style={{ width: '100%' }}>
-                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
-                                                    Price
-                                                </Text>
-                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                                    ₱ Titesasdasd.00
-                                                </Text>
-                                            </View>
-                                        </View>
-                                    </View>
 
-                                    <View style={{ width: '100%', height: '30%', padding: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <TouchableOpacity style={{ width: '47%', height: '100%', backgroundColor: Colors.semiblack, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
-                                            <Text
-                                                style={{ color: Colors.whiteColor, fontSize: height * 0.02 }}
-                                            >
-                                                Decline
-                                            </Text>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity 
-                                        onPress={handleViewOrder}
-                                        style={{ width: '47%', height: '100%', backgroundColor: Colors.orange, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
-                                            <Text
-                                                style={{ color: Colors.whiteColor, fontSize: height * 0.02 }}
-                                            >
-                                                View Order
-                                            </Text>
-                                        </TouchableOpacity>
+                                        <View style={{ width: '100%', height: '30%', padding: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                            <TouchableOpacity style={{ width: '47%', height: '100%', backgroundColor: Colors.semiblack, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
+                                                <Text
+                                                    style={{ color: Colors.whiteColor, fontSize: height * 0.02 }}
+                                                >
+                                                    Decline
+                                                </Text>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity
+                                                onPress={handleViewOrder}
+                                                style={{ width: '47%', height: '100%', backgroundColor: Colors.orange, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
+                                                <Text
+                                                    style={{ color: Colors.whiteColor, fontSize: height * 0.02 }}
+                                                >
+                                                    View Order
+                                                </Text>
+                                            </TouchableOpacity>
+                                        </View>
                                     </View>
                                 </View>
-                            </View>
+                            ))}
+
                         </View>
                     </View>
                 </ScrollView>
