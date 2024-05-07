8ac3b5ce3198838ae9eb75d3374e33eb16e27b1d
Marc Edison <suanieeee@yahoo.com>
2024-05-07 23:55:44 +0800

    feat: Add navigation functionality for Cancelled, Unreturned, and Reviews screens

--- Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/ToShip.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/ToShip.jsx
@@ -8,12 +8,19 @@ import {
     Image
 } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import * as Colors from '../../../../utils/colors'
 import Navbar from '../../../components/Navbar'
 
 const { width, height } = Dimensions.get('window')
 
 export default function ToShip() {
+    const navigation = useNavigation()
+
+    const handleViewOrder = () => {
+        navigation.navigate('Summary')
+    }
+
     return (
         <>
             <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
@@ -22,63 +29,63 @@ export default function ToShip() {
                 <ScrollView>
                     <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
                         <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01 }}>
-                            <Text style={{ fontWeight: '600', fontSize: height * 0.02 }}>
+                            <Text style={{ fontWeight: '600', fontSize: height * 0.017 }}>
                                 Pending Orders
                             </Text>
-                            <View style={{ width: '100%', gap: height * 0.03 }}>
-                                <View
-                                    style={{
-                                        width: '100%',
-                                        height: height * 0.20,
-                                        borderRadius: height * 0.01,
-                                        overflow: 'hidden',
-                                        padding: height * 0.01,
-                                        backgroundColor: Colors.idleColor,
-
-                                    }}
-                                >
-                                    <View style={{ width: '100%', height: '80%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', gap: height * 0.03 }}>
+                            <View style={{ width: '100%', backgroundColor: Colors.idleColor, borderRadius: height * 0.01 }}>
+                                <View style={{ overflow: 'hidden', width: '100%', height: height * 0.27, padding: height * 0.01, justifyContent: 'space-between', alignItems: 'center' }}>
+                                    <View style={{ width: '100%', height: '70%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', }}>
                                         <View style={{ overflow: 'hidden', width: '40%', height: '100%', backgroundColor: Colors.fontColor, borderRadius: height * 0.01 }}>
                                             <Image
-                                                source={{ uri: 'https://source.unsplash.com/woman-stretching-her-arm-upward-8HqPXTToMn0' }}
+                                                source={{ uri: 'http://source.unsplash.com/woman-standing-on-railing-w6Lb5Px8a6g' }}
                                                 resizeMode='cover'
                                                 style={{ width: '100%', height: '100%' }}
                                             />
                                         </View>
                                         <View style={{ width: '60%', height: '100%', padding: height * 0.01, justifyContent: 'center', gap: height * 0.01 }}>
-                                            <View>
-                                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
+                                            <View style={{ width: '100%' }}>
+                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                                     Product Name
                                                 </Text>
-                                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                                    The Product Nma
+                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                                    Titesasdasd
                                                 </Text>
                                             </View>
-                                            <View>
-                                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
+                                            <View style={{ width: '100%' }}>
+                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                                     Store Name
                                                 </Text>
-                                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                                    The Shop Nameasdasd
+                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                                    Titesasdasd
                                                 </Text>
                                             </View>
-                                            <View>
-                                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
+                                            <View style={{ width: '100%' }}>
+                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', fontWeight: 'bold' }}>
                                                     Price
                                                 </Text>
-                                                <Text style={{ color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
-                                                    ₱ 0001.00
+                                                <Text style={{ width: '100%', color: Colors.fontColor, textAlign: 'justify', paddingHorizontal: width * 0.01 }} numberOfLines={1} ellipsizeMode='tail'>
+                                                    ₱ Titesasdasd.00
                                                 </Text>
                                             </View>
                                         </View>
                                     </View>
-                                    <View
-                                        style={{ width: '100%', height: '15%', backgroundColor: 'black' }}
-                                    >
-                                        <TouchableOpacity
-                                            style={{ width: '40%' }}
-                                        >
-                                            <Text></Text>
+
+                                    <View style={{ width: '100%', height: '30%', padding: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                        <TouchableOpacity style={{ width: '47%', height: '100%', backgroundColor: Colors.semiblack, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
+                                            <Text
+                                                style={{ color: Colors.whiteColor, fontSize: height * 0.02 }}
+                                            >
+                                                Decline
+                                            </Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity 
+                                        onPress={handleViewOrder}
+                                        style={{ width: '47%', height: '100%', backgroundColor: Colors.orange, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
+                                            <Text
+                                                style={{ color: Colors.whiteColor, fontSize: height * 0.02 }}
+                                            >
+                                                View Order
+                                            </Text>
                                         </TouchableOpacity>
                                     </View>
                                 </View>
