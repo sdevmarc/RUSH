4ee94127b9ae1ddcdee5fa196bf0d8aa2c21e794
sdevmarc <suanieeee@yahoo.com>
2024-05-07 22:34:33 +0800

    feat: Add ViewTransactions and UpdateStatusTransaction methods
    This commit adds the ViewTransactions and UpdateStatusTransaction methods to the Transactions controller. The ViewTransactions method retrieves all transactions from the database and returns them as a response. The UpdateStatusTransaction method updates the status of a transaction based on the provided transactionId and status parameters.

--- Backend/server/controllers/Transactions.js
+++ Backend/server/controllers/Transactions.js
@@ -7,14 +7,42 @@ const TransactionController = {
 
             const data = await Transactions.create(values)
             console.log(data)
-            if(data) {
+            if (data) {
                 res.json({ success: true, message: `Transaction added successfully!` })
             } else {
                 res.json({ success: false, message: `Transaction not added!` })
             }
-      
+
         } catch (error) {
             res.json({ success: false, message: `Error creating Transaction controller: ${error}` })
+        }
+    },
+    ViewTransactions: async (req, res) => {
+        try {
+            const data = await Transactions.find()
+            console.log(data)
+
+            if (data) {
+                res.json({ success: true, message: `Transactions retrieved successfully!`, data })
+            } else {
+                res.json({ success: false, message: `There are no transactions made.` })
+            }
+        } catch (error) {
+            res.json({ success: false, message: `Error viewing status Transaction controller: ${error}` })
+        }
+    },
+    UpdateStatusTransaction: async (req, res) => {
+        try {
+            const { transactionId, status } = req.params
+
+            const data = await Transactions.findByIdAndUpdate(transactionId, { "checkout.status": `${status}` }, { new: true })
+            if (data) {
+                res.json({ success: true, message: `Transactions updated to ${status} successfully!`, data })
+            } else {
+                res.json({ success: false, message: `Transactions did not updated to ${status}.` })
+            }
+        } catch (error) {
+            res.json({ success: false, message: `Error Updating status to ${status} Transaction controller: ${error}` })
         }
     }
 }
