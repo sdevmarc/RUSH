c768d9f523a1c80f9e1515ccfb2e83263281c26d
Marc Edison <suanieeee@yahoo.com>
2024-05-08 01:50:21 +0800

    feat: Update viewtransactions route to include sellerId parameter

--- Backend/server/routes/Transactions.js
+++ Backend/server/routes/Transactions.js
@@ -3,7 +3,7 @@ const Transaction = require('../controllers/Transactions')
 const router = express.Router()
 
 router.post('/createtransaction', Transaction.CreateTransaction)
-router.get('/viewtransactions', Transaction.ViewTransactions)
+router.get('/viewtransactions/:sellerId', Transaction.ViewTransactions)
 router.post('/updatestatustocancelled/:transactionId', Transaction.UpdateStatusTransaction)
 
 module.exports = router
