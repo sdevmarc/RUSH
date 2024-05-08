137394a75b0ba99d3f5fafeff1ef0ee5e82227e8
Marc Edison <suanieeee@yahoo.com>
2024-05-09 03:27:27 +0800

    feat: Add CustomerService screen to Settings module

--- Frontend/Mobile/reclient/app/screens/Settings/Orders.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/Orders.jsx
@@ -4,31 +4,33 @@ import {
     StatusBar,
     ScrollView,
     Dimensions,
-    TouchableOpacity,
-    Image
+    TouchableOpacity
 } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
-import * as Color from '../../../utils/colors'
+import { MaterialCommunityIcons, Feather, Ionicons } from '@expo/vector-icons'
+import * as Colors from '../../../utils/colors'
 
 const { width, height } = Dimensions.get('window')
 
 const Orders = () => {
     const navigation = useNavigation()
 
+    const handleOpenDrawer = () => {
+        navigation.openDrawer()
+    }
+
     return (
         <>
-            <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
-            <View style={{ width: width, height: height, backgroundColor: '#323d48' }}>
+            <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+            <View style={{ width: width, height: height, backgroundColor: Colors.backgroundColor }}>
                 <View
                     style={{
                         position: 'absolute',
                         width: '100%',
                         height: height * 0.12,
                         zIndex: 1,
-                        backgroundColor: '#323d48',
-                        paddingVertical: height * 0.009
+                        paddingVertical: height * 0.009,
                     }}
                 >
                     <View
@@ -44,62 +46,106 @@ const Orders = () => {
                         }}
                     >
                         <TouchableOpacity
-                            onPress={() => navigation.openDrawer()}
+                            onPress={handleOpenDrawer}
                         >
-                            <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="white" />
+                            <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="black" />
                         </TouchableOpacity>
                     </View>
                     <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
-                        <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
+                        <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: Colors.fontColor }}>
                             Orders
                         </Text>
                     </View>
                 </View>
                 <ScrollView>
-                    <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
-                        <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01, paddingBottom: height * 0.2 }}>
-                            <TouchableOpacity style={{ width: '100%', height: height * 0.23, backgroundColor: Color.secondaryColor, borderRadius: height * 0.01, overflow: 'hidden' }}>
-                                <View style={{ width: '100%', height: '75%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.02 }}>
-                                    <View style={{ width: '40%', height: '90%', overflow: 'hidden', backgroundColor: 'black', borderRadius: height * 0.01 }}>
-                                        <Image source={{ uri: 'source.unsplash.com/a-man-with-pink-hair-looking-up-at-the-sky-kY_0Xv0_rNs' }} style={{ width: '100%', height: '100%' }} resizeMode='cover' />
-                                    </View>
-                                    <View style={{ width: '60%', flexDirection: 'rwo', paddingHorizontal: width * 0.02, gap: height * 0.01 }}>
-                                        <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-between' }}>
-                                            <Text style={{ color: 'white', fontWeight: '500' }}>
-                                                Product Name:
-                                            </Text>
-                                            <Text style={{ width: '100%', color: 'white' }} numberOfLines={1} ellipsizeMode='tail' >
-                                                The Product
-                                            </Text>
-                                        </View>
-                                        <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-between' }}>
-                                            <Text style={{ color: 'white', fontWeight: '500' }}>
-                                                Store Name:
-                                            </Text>
-                                            <Text style={{ width: '100%', color: 'white' }} numberOfLines={1} ellipsizeMode='tail' >
-                                                The Product
-                                            </Text>
-                                        </View>
-                                        <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-between' }}>
-                                            <Text style={{ color: 'white', fontWeight: '500' }}>
-                                                Total Fee:
-                                            </Text>
-                                            <Text style={{ width: '100%', color: 'white' }} numberOfLines={1} ellipsizeMode='tail' >
-                                                P 100.00
-                                            </Text>
-                                        </View>
-                                    </View>
-                                </View>
-                                <View style={{ width: '100%', height: '20%', paddingHorizontal: width * 0.02 }}>
-                                    <TouchableOpacity style={{ width: '100%', height: '100%', backgroundColor: Color.accent, justifyContent: 'center', alignItems: 'center', borderRadius: height * 0.01 }}>
-                                        <Text style={{ fontWeight: 'bold', color: 'white' }}>
-                                            RATE ITEM
-                                        </Text>
-                                    </TouchableOpacity>
-                                </View>
+                    <View style={{ width: width, height: height, paddingHorizontal: width * 0.03, paddingTop: height * 0.13, gap: height * 0.01 }}>
+                        <View style={{ width: '100%', gap: height * 0.01 }}>
+                            <Text style={{ fontWeight: '600', fontSize: height * 0.02 }}>
+                                Purchases
+                            </Text>
+                            <TouchableOpacity
+
+                                style={{ width: '100%', height: height * 0.09, backgroundColor: Colors.idleColor, borderRadius: height * 0.02, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    To Ship
+                                </Text>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    0
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+
+                                style={{ width: '100%', height: height * 0.09, backgroundColor: Colors.idleColor, borderRadius: height * 0.02, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    Cancelled
+                                </Text>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    0
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+
+                                style={{ width: '100%', height: height * 0.09, backgroundColor: Colors.idleColor, borderRadius: height * 0.02, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    Unreturned
+                                </Text>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    0
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+
+                                style={{ width: '100%', height: height * 0.09, backgroundColor: Colors.idleColor, borderRadius: height * 0.02, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    Reviews
+                                </Text>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.02, fontWeight: '600' }}>
+                                    0
+                                </Text>
                             </TouchableOpacity>
                         </View>
-                    </View >
+                        <View style={{ width: '100%', gap: height * 0.01 }}>
+                            <Text style={{ fontWeight: '600', fontSize: height * 0.02 }}>
+                                Others
+                            </Text>
+                            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap: width * 0.03 }}>
+                                <TouchableOpacity
+                                    style={{
+                                        width: width * 0.29,
+                                        height: height * 0.13,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        backgroundColor: Colors.idleColor,
+                                        borderRadius: height * 0.02,
+                                        gap: height * 0.01
+                                    }}
+                                >
+                                    <Feather name="shopping-bag" size={24} color="black" />
+                                    <Text style={{ width: '80%', color: Colors.fontColor, fontWeight: '600', textAlign: 'center' }}>
+                                        My Likes
+                                    </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={{
+                                        width: width * 0.29,
+                                        height: height * 0.13,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        backgroundColor: Colors.idleColor,
+                                        borderRadius: height * 0.02,
+                                        gap: height * 0.01
+                                    }}
+                                >
+                                    <Ionicons name="analytics" size={24} color="black" />
+                                    <Text style={{ width: '80%', color: Colors.fontColor, fontWeight: '600', textAlign: 'center' }}>
+                                        Completed Orders
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+
+
+                        </View>
+                    </View>
                 </ScrollView >
             </View >
         </>
