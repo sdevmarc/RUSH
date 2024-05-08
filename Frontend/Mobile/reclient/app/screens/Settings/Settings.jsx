7a6310841f5cc60e44bc60e7b552c9e9e643e827
Marc Edison <suanieeee@yahoo.com>
2024-05-09 03:27:33 +0800

    feat: Add CustomerService screen to Settings module

--- Frontend/Mobile/reclient/app/screens/Settings/Settings.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/Settings.jsx
@@ -8,60 +8,105 @@ import {
 } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
+import { MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons'
+import * as Colors from '../../../utils/colors'
 
 const { width, height } = Dimensions.get('window')
 
 const Settings = () => {
     const navigation = useNavigation()
 
+    const handleOpenDrawer = () => {
+        navigation.openDrawer()
+    }
+
     return (
         <>
-            <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
-            <View
-                style={{
-                    position: 'absolute',
-                    width: '100%',
-                    height: height * 0.12,
-                    zIndex: 1,
-                    backgroundColor: '#323d48',
-                    paddingVertical: height * 0.009
-                }}
-            >
+            <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+            <View style={{ width: width, height: height, backgroundColor: Colors.backgroundColor }}>
                 <View
                     style={{
+                        position: 'absolute',
                         width: '100%',
-                        height: '100%',
-                        paddingHorizontal: width * 0.05,
-                        width: width,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'flex-end',
-                        zIndex: 1
+                        height: height * 0.12,
+                        zIndex: 1,
+                        paddingVertical: height * 0.009,
+                        backgroundColor: Colors.backgroundColor
                     }}
                 >
-                    <TouchableOpacity
-                        onPress={() => navigation.openDrawer()}
+                    <View
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            paddingHorizontal: width * 0.05,
+                            width: width,
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'flex-end',
+                            zIndex: 1
+                        }}
                     >
-                        <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="white" />
-                    </TouchableOpacity>
-                </View>
-                <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
-                    <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: 'white' }}>
-                        Settings
-                    </Text>
-                </View>
-            </View>
-            <ScrollView>
-                <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
-                    <View style={{ width: '100%', paddingTop: height * 0.1, gap: height * 0.01 }}>
-                        <Text>
-                            asdasd
+                        <TouchableOpacity
+                            onPress={handleOpenDrawer}
+                        >
+                            <MaterialCommunityIcons name="dots-grid" size={width * 0.08} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ width: '100%', height: '100%', position: 'absolute', justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <Text style={{ fontSize: width * 0.05, fontWeight: 'bold', color: Colors.fontColor }}>
+                            Settings
                         </Text>
                     </View>
                 </View>
-            </ScrollView>
-
+                <ScrollView>
+                    <View style={{ width: width, height: height, paddingHorizontal: width * 0.03, paddingTop: height * 0.13, gap: height * 0.01 }}>
+                        <View style={{ width: '100%', gap: height * 0.01 }}>
+                            <TouchableOpacity
+                                style={{ width: '100%', height: height * 0.07, backgroundColor: Colors.idleColor, borderRadius: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.016, fontWeight: '600' }}>
+                                    Notification Settings
+                                </Text>
+                                <SimpleLineIcons name="arrow-right" size={20} color="black" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ width: '100%', height: height * 0.07, backgroundColor: Colors.idleColor, borderRadius: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.016, fontWeight: '600' }}>
+                                    Privacy Settings
+                                </Text>
+                                <SimpleLineIcons name="arrow-right" size={20} color="black" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ width: '100%', height: height * 0.07, backgroundColor: Colors.idleColor, borderRadius: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.016, fontWeight: '600' }}>
+                                    Community Policies
+                                </Text>
+                                <SimpleLineIcons name="arrow-right" size={20} color="black" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ width: '100%', height: height * 0.07, backgroundColor: Colors.idleColor, borderRadius: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.016, fontWeight: '600' }}>
+                                    RUSh Policies
+                                </Text>
+                                <SimpleLineIcons name="arrow-right" size={20} color="black" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ width: '100%', height: height * 0.07, backgroundColor: Colors.idleColor, borderRadius: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.016, fontWeight: '600' }}>
+                                    Give us a feedback!
+                                </Text>
+                                <SimpleLineIcons name="arrow-right" size={20} color="black" />
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ width: '100%', height: height * 0.07, backgroundColor: Colors.idleColor, borderRadius: height * 0.01, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: width * 0.05 }}>
+                                <Text style={{ color: Colors.fontColor, fontSize: height * 0.016, fontWeight: '600' }}>
+                                    Logout
+                                </Text>
+                                <SimpleLineIcons name="arrow-right" size={20} color="black" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView >
+            </View >
         </>
     )
 }
