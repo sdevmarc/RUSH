2eb2667a739abe7fcda0a45b4ecd87bdb64308b8
Marc Edison <suanieeee@yahoo.com>
2024-05-09 02:12:07 +0800

    feat: Add ViewStatusTransactions and ViewSelectedTransaction routes
    This commit adds the ViewStatusTransactions and ViewSelectedTransaction routes to the Transactions module. The ViewStatusTransactions route retrieves transactions based on the sellerId and status parameters, returning the transactions along with the associated product and store details. The ViewSelectedTransaction route retrieves a specific transaction based on the transactionId, returning the transaction, product, store, and user details. These new routes enhance the functionality of the Transactions module and improve the user experience by providing more comprehensive transaction views.

--- Backend/server/routes/Transactions.js
+++ Backend/server/routes/Transactions.js
@@ -3,7 +3,9 @@ const Transaction = require('../controllers/Transactions')
 const router = express.Router()
 
 router.post('/createtransaction', Transaction.CreateTransaction)
+router.get('/viewstatustransactions/:sellerId/:status', Transaction.ViewStatusTransactions)
 router.get('/viewtransactions/:sellerId', Transaction.ViewTransactions)
-router.post('/updatestatustocancelled/:transactionId', Transaction.UpdateStatusTransaction)
+router.post('/updatetransactionstatus', Transaction.UpdateStatusTransaction)
+router.get('/viewselectedtransaction/:transactionId', Transaction.ViewSelectedTransaction)
 
 module.exports = router
