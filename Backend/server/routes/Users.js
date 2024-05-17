52cf582e86abec13193d9d7f88772a53eecbb938
sdevmarc <suanieeee@yahoo.com>
2024-05-17 15:50:34 +0800

    refactor: Add search and get all users functionality to UserController
    This commit refactors the UserController in the Users.js file to include new functionality for searching users and retrieving all users. It adds the SearchUsers and GetAllUsers methods, improving the user management capabilities of the application.

--- Backend/server/routes/Users.js
+++ Backend/server/routes/Users.js
@@ -5,7 +5,9 @@ const router = express.Router()
 router.get('/getuser/:userId', Users.FetchUser)
 router.get('/getaddress/:userId', Users.fetchAddress)
 router.get('/getactiveaddress/:userId', Users.fetchActiveAddrees)
-router.post('/addaddress',Users.AddPersonalDetails, Users.AddAddress)
+router.post('/addaddress', Users.AddPersonalDetails, Users.AddAddress)
 router.post('/activeaddress', Users.UpdateActiveAddress)
+router.get('/searchuser/:searchId', Users.SearchUsers)
+router.get('/getalluser', Users.GetAllUsers)
 
 module.exports = router
