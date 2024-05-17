4baf4903c192df966f0cd159645f7f9e60ab7511
Marc Edison <suanieeee@yahoo.com>
2024-05-18 00:03:01 +0800

    refactor: Update Loading component styles
    The code changes in the Loading.jsx file update the styles of the Loading component. This refactor improves the visual appearance of the component by adjusting the width, height, background color, and text font weight.

--- Frontend/Mobile/reclient/app/components/Loading.jsx
+++ Frontend/Mobile/reclient/app/components/Loading.jsx
@@ -6,10 +6,10 @@ const { width, height } = Dimensions.get('window')
 
 export default function Loading({ title }) {
     return (
-        <SafeAreaView style={{ position: 'absolute', zIndex: 1, width: '100%', height: '100%', backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center' }}>
+        <SafeAreaView style={{ position: 'absolute', zIndex: 1, width: width, height: height, backgroundColor: 'rgba(0,0,0,0.2)', justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ width: width * 0.27, height: height * 0.12, borderRadius: height * 0.02, backgroundColor: Colors.idleColor, justifyContent: 'center', alignItems: 'center', gap: height * 0.01 }}>
                 <ActivityIndicator size="large" />
-                <Text style={{ fontWeight: 'bold', fontSize: height * 0.013 }}>
+                <Text style={{ fontWeight: '600', fontSize: height * 0.013 }}>
                     {title}
                 </Text>
             </View>
