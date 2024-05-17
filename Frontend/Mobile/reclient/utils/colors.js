f21a813bd9bf1858717c35f0509244876fa3f33d
sdevmarc <suanieeee@yahoo.com>
2024-05-17 19:41:00 +0800

    refactor: Update black color constant in colors.js
    The code changes in the colors.js file update the black color constant from '#000000' to '#222222'. This modification ensures consistency in the color scheme throughout the application.

--- Frontend/Mobile/reclient/utils/colors.js
+++ Frontend/Mobile/reclient/utils/colors.js
@@ -1,7 +1,7 @@
 export const backgroundColor = '#ffffff'
 export const fontColor = '#000'
 export const whiteColor = '#ffffff'
-export const black = '#000000'
+export const black = '#222222'
 export const semiblack = '#333333'
 export const idleColor = '#EFEFEF'
 export const orange = '#E1793C'
