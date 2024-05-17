84ed36f32498411d21fd1cfc680584f8d56a5d3c
sdevmarc <suanieeee@yahoo.com>
2024-05-17 15:37:12 +0800

    feat: Add search functionality for products
    The code changes in the `Product.js` file add the `SearchProduct` function, which allows users to search for products based on the store ID and a search ID. This commit enhances the product functionality by enabling users to search for specific products.

--- Backend/server/routes/Product.js
+++ Backend/server/routes/Product.js
@@ -6,5 +6,6 @@ const Products = require('../controllers/Products')
 router.post('/addproduct', Products.AddProduct)
 router.get('/getproducts/:storeId', Products.FetchProducts)
 router.get('/selectproduct/:id', Products.SelectedItem)
+router.get('/searchproduct/:storeId/:searchId', Products.SearchProduct)
 
 module.exports = router
