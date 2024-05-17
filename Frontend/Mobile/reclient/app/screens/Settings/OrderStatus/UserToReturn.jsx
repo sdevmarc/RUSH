e3be2ad1f4f5c24a8efdd772dd97385cd2c7b4ba
Marc Edison <suanieeee@yahoo.com>
2024-05-18 00:04:33 +0800

    refactor: Add loading state to UserToReturn screen

--- Frontend/Mobile/reclient/app/screens/Settings/OrderStatus/UserToReturn.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/OrderStatus/UserToReturn.jsx
@@ -14,24 +14,33 @@ import Navbar from '../../../components/Navbar'
 import axios from 'axios'
 import address from '../../../../config/host'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import Loading from '../../../components/Loading'
 
 const { width, height } = Dimensions.get('window')
 
 export default function UserToReturn() {
     const [values, setValues] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useFocusEffect(useCallback(() => {
         fetchUnreturned()
     }, []))
 
     const fetchUnreturned = async () => {
-        const userId = await AsyncStorage.getItem('userId')
-        const res = await axios.get(`http:${address}/api/viewstatustransactions/${userId}/user/UNRETURNED`)
-
-        if (res?.data?.success) {
-            setValues(res?.data?.data)
-        } else {
-            Alert.alert(res?.data?.message)
+        try {
+            setIsLoading(true)
+            const userId = await AsyncStorage.getItem('userId')
+            const res = await axios.get(`http:${address}/api/viewstatustransactions/${userId}/user/UNRETURNED`)
+    
+            if (res?.data?.success) {
+                setValues(res?.data?.data)
+            } else {
+                console.log(res?.data?.message)
+            }
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -39,6 +48,7 @@ export default function UserToReturn() {
         <>
             <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
             <View style={{ width: width, height: height, backgroundColor: Colors.backgroundColor }}>
+            {isLoading && <Loading title={`Loading`} />}
                 <Navbar title='Unreturned Orders' backgroundColor={Colors.backgroundColor} tintColor={Colors.fontColor} />
                 <ScrollView>
                     <View style={{ width: width, paddingHorizontal: width * 0.03, paddingVertical: height * 0.03 }}>
