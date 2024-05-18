08cd1125e576128c238b79564d7afea3bcbf0079
sdevmarc <suanieeee@yahoo.com>
2024-05-18 17:52:56 +0800

    refactor: Fix image loading issue in Product.jsx

--- Frontend/Mobile/reclient/app/screens/Settings/Account.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/Account.jsx
@@ -5,16 +5,18 @@ import {
     StatusBar,
     TouchableOpacity,
     ScrollView,
-    ImageBackground
+    ImageBackground,
+    Alert
 } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { MaterialCommunityIcons, MaterialIcons, Entypo } from '@expo/vector-icons'
 import axios from 'axios'
 import address from '../../../config/host'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as Colors from '../../../utils/colors'
 import Loading from '../../components/Loading'
+import * as ImagePicker from 'expo-image-picker'
 
 const { width, height } = Dimensions.get('window')
 
@@ -22,10 +24,16 @@ const Account = () => {
     const navigation = useNavigation()
     const [isLoading, setIsLoading] = useState(false)
     const [values, setValues] = useState([])
+    const [isEdit, setEdit] = useState(false)
+    const [isImage, setImage] = useState('')
+    const [details, setDetails] = useState({
+        userId: '',
+        profilePhoto: ''
+    })
 
-    useEffect(() => {
+    useFocusEffect(useCallback(() => {
         fetchData()
-    }, [])
+    }, []))
 
     const fetchData = async () => {
         try {
@@ -38,7 +46,6 @@ const Account = () => {
         } finally {
             setIsLoading(false)
         }
-
     }
 
     const handleOpenDrawer = () => {
@@ -52,7 +59,86 @@ const Account = () => {
 
     const handleStartRenting = () => {
         navigation.navigate('ShopInformation')
+    }
 
+    const handleSaveEditedPhoto = () => {
+        setIsLoading(true)
+        handleUploadImage(isImage)
+    }
+
+    const handleUploadImage = async (value) => {
+        try {
+            setIsLoading(true)
+            const userId = await AsyncStorage.getItem('userId')
+            const data = new FormData();
+            data.append('file', {
+                uri: value,
+                name: `photo.${value.split('.').pop()}`,
+                type: `image/${value.split('.').pop()}`
+            })
+            data.append('upload_preset', '_products')
+            data.append('cloud_name', 'do1p9llzd')
+            data.append('folder', 'user_photo')
+
+            const res = await axios.post(`https://api.cloudinary.com/v1_1/do1p9llzd/image/upload`, data, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+
+            if (res) {
+                const updateUserPhoto = await axios.post(`http://${address}/api/updateprofilephoto`, { userId, profilePhoto: res?.data?.url })
+
+                if (updateUserPhoto) {
+                    Alert.alert(updateUserPhoto?.data?.message)
+                } else {
+                    console.log('Error', updateUserPhoto?.data?.message)
+                }
+            } else {
+                console.log(`Error data`)
+            }
+        } catch (error) {
+            console.log(`Error Handle Image Upload ${error}`)
+        } finally {
+            setImage('')
+            setEdit(false)
+            setIsLoading(false)
+            fetchData()
+        }
+    }
+
+    const handleEditPhoto = async () => {
+        try {
+            setIsLoading(true)
+            setEdit(true)
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 1,
+            })
+
+            if (!result.canceled) {
+                try {
+                    setImage(result.assets[0].uri)
+                } catch (error) {
+                    console.log(`Error: ${error}`)
+                }
+            } else {
+                setEdit(false)
+            }
+
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const handleCancel = () => {
+        setEdit(false)
+        setImage('')
     }
 
     return (
@@ -60,17 +146,41 @@ const Account = () => {
             <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
             <View style={{ width: width, backgroundColor: Colors.backgroundColor }}>
                 {isLoading && <Loading title={`Loading`} />}
-                <ImageBackground source={{ uri: 'https://source.unsplash.com/man-wearing-black-notched-lapel-suit-jacket-in-focus-photography-WMD64tMfc4k' }} style={{ width: '100%', height: height * 0.25, }} resizeMode='cover'>
+                <ImageBackground source={{ uri: isImage ? isImage : values?.profilePhoto }} style={{ width: '100%', height: height * 0.25, }} resizeMode='cover'>
                     <View style={{ width: '100%', height: '100%', justifyContent: 'flex-end', alignItems: 'flex-start', padding: width * 0.05, backgroundColor: 'rgba(0,0,0,0.2)' }}>
                         <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                             <Text style={{ color: Colors.whiteColor, fontWeight: '700', fontSize: width * 0.05 }}>
                                 {values.displayName}
                             </Text>
-                            <TouchableOpacity style={{ paddingHorizontal: width * 0.03, paddingVertical: height * 0.008, backgroundColor: Colors.orange }}>
-                                <Text style={{ color: Colors.whiteColor, fontWeight: '600', fontSize: width * 0.03 }}>
-                                    Edit Photo
-                                </Text>
-                            </TouchableOpacity>
+
+                            {
+                                isEdit ? (
+                                    <View style={{ gap: height * 0.01 }}>
+                                        <TouchableOpacity
+                                            onPress={() => handleSaveEditedPhoto()}
+                                            style={{ paddingHorizontal: width * 0.07, paddingVertical: height * 0.006, backgroundColor: Colors.orange, justifyContent: 'center', alignItems: 'center' }}>
+                                            <Text style={{ color: Colors.whiteColor, fontWeight: '600', fontSize: width * 0.03 }}>
+                                                Save
+                                            </Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity
+                                            onPress={handleCancel}
+                                            style={{ paddingHorizontal: width * 0.03, paddingVertical: height * 0.006, backgroundColor: Colors.idleColor, justifyContent: 'center', alignItems: 'center' }}>
+                                            <Text style={{ color: Colors.fontColor, fontWeight: '600', fontSize: width * 0.03 }}>
+                                                Cancel
+                                            </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                ) : (
+                                    <TouchableOpacity
+                                        onPress={handleEditPhoto}
+                                        style={{ paddingHorizontal: width * 0.03, paddingVertical: height * 0.006, backgroundColor: Colors.orange, justifyContent: 'center', alignItems: 'center' }}>
+                                        <Text style={{ color: Colors.whiteColor, fontWeight: '600', fontSize: width * 0.03 }}>
+                                            Edit Photo
+                                        </Text>
+                                    </TouchableOpacity>
+                                )
+                            }
                         </View>
                         <View style={{ width: '100%', flexDirection: 'row', gap: width * 0.03 }}>
                             <Text style={{ color: Colors.whiteColor, fontWeight: '600', fontSize: width * 0.03 }}>
@@ -165,7 +275,7 @@ const Account = () => {
                             <Text style={{ fontWeight: '600', fontSize: height * 0.02 }}>
                                 Others
                             </Text>
-                            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start', gap: width * 0.03 }}>
+                            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'flex-start', gap: width * 0.03 }}>
                                 <TouchableOpacity
                                     onPress={handleAddress}
                                     style={{
@@ -183,23 +293,27 @@ const Account = () => {
                                         My Addresses
                                     </Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity
-                                    onPress={handleStartRenting}
-                                    style={{
-                                        width: width * 0.29,
-                                        height: height * 0.13,
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        backgroundColor: Colors.idleColor,
-                                        borderRadius: height * 0.02,
-                                        gap: height * 0.01
-                                    }}
-                                >
-                                    <MaterialIcons name="sell" size={24} color="black" />
-                                    <Text style={{ width: '80%', color: Colors.fontColor, fontWeight: '600', textAlign: 'center' }}>
-                                        Start Renting
-                                    </Text>
-                                </TouchableOpacity>
+                                {
+                                    values.UserType === 'Rentee' && (
+                                        <TouchableOpacity
+                                            onPress={handleStartRenting}
+                                            style={{
+                                                width: width * 0.29,
+                                                height: height * 0.13,
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                backgroundColor: Colors.idleColor,
+                                                borderRadius: height * 0.02,
+                                                gap: height * 0.01
+                                            }}
+                                        >
+                                            <MaterialIcons name="sell" size={24} color="black" />
+                                            <Text style={{ width: '80%', color: Colors.fontColor, fontWeight: '600', textAlign: 'center' }}>
+                                                Start Renting
+                                            </Text>
+                                        </TouchableOpacity>
+                                    )
+                                }
                                 <TouchableOpacity
                                     style={{
                                         width: width * 0.29,
