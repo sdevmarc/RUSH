b67682698994c0c03eeac0c6e2daa6be98ef9820
sdevmarc <suanieeee@yahoo.com>
2024-05-19 20:57:53 +0800

    refactor: Remove unnecessary code in UserCancelled.jsx

--- Frontend/Mobile/reclient/app/screens/Settings/OrderStatus/UserCancelled.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/OrderStatus/UserCancelled.jsx
@@ -4,8 +4,7 @@ import {
     StatusBar,
     Dimensions,
     ScrollView,
-    Image,
-    Alert
+    Image
 } from 'react-native'
 import React, { useCallback, useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native'
