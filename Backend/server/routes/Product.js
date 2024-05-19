40870e28f4b1376f69e7b38fa9427b7f28ca0779
Marc Edison <suanieeee@yahoo.com>
2024-05-19 15:21:35 +0800

    refactor: Update selectproduct route to include IsDelivery parameter

--- Backend/server/routes/Product.js
+++ Backend/server/routes/Product.js
@@ -5,7 +5,7 @@ const Products = require('../controllers/Products')
 
 router.post('/addproduct', Products.AddProduct)
 router.get('/getproducts/:storeId', Products.FetchProducts)
-router.get('/selectproduct/:id', Products.SelectedItem)
+router.get('/selectproduct/:id/:IsDelivery', Products.SelectedItem)
 router.get('/searchproduct/:storeId/:searchId', Products.SearchProduct)
 
 module.exports = router
