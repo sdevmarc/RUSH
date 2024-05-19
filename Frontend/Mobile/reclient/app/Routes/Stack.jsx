36cfebac66919cd2ffe11a0f43201f37278f9c06
sdevmarc <suanieeee@yahoo.com>
2024-05-19 20:57:04 +0800

    feat: Add UserCompleted and Completed screens, and SellerViewProduct component
    Add UserCompleted screen to the Settings/OrderStatus folder and Completed screen to the Seller/OrderStatus folder. Also, add SellerViewProduct component to the Seller/Add Products folder. These changes introduce new screens and components to enhance the user experience and provide additional functionality. The commit message follows the established convention of prefixing the commit type (feat) followed by a concise and descriptive message.

--- Frontend/Mobile/reclient/app/Routes/Stack.jsx
+++ Frontend/Mobile/reclient/app/Routes/Stack.jsx
@@ -26,6 +26,9 @@ import UserToReturn from '../screens/Settings/OrderStatus/UserToReturn'
 import UserToReview from '../screens/Settings/OrderStatus/UserToReview'
 import EditAccountDetails from '../screens/Settings/Profile/EditAccountDetails'
 import VisitShop from '../screens/Seller/VisitShop'
+import UserCompleted from '../screens/Settings/OrderStatus/UserCompleted'
+import Completed from '../screens/Seller/OrderStatus/Completed'
+import SellerViewProduct from '../screens/Seller/Add Products/SellerViewProduct'
 
 const Stack = createNativeStackNavigator()
 
@@ -55,13 +58,16 @@ function StackRoutes() {
                 <Stack.Screen name="UserCancelled" component={UserCancelled} options={{ headerTitle: 'UserCancelled', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="UserToReturn" component={UserToReturn} options={{ headerTitle: 'UserToReturn', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="UserToReview" component={UserToReview} options={{ headerTitle: 'UserToReview', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="UserCompleted" component={UserCompleted} options={{ headerTitle: 'UserCompleted', headerTitleAlign: 'center', headerShown: false }} />
                 {/* SELLER */}
                 <Stack.Screen name="Products" component={Product} options={{ headerTitle: 'Products', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="AddProduct" component={AddProducts} options={{ headerTitle: 'AddProducts', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="SellerViewProduct" component={SellerViewProduct} options={{ headerTitle: 'SellerViewProduct', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="ToShip" component={ToShip} options={{ headerTitle: 'ToShip', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Summary" component={Summary} options={{ headerTitle: 'Summary', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Cancelled" component={Cancelled} options={{ headerTitle: 'Cancelled', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Unreturned" component={Unreturned} options={{ headerTitle: 'Unreturned', headerTitleAlign: 'center', headerShown: false }} />
+                <Stack.Screen name="Completed" component={Completed} options={{ headerTitle: 'Unreturned', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="Reviews" component={Reviews} options={{ headerTitle: 'Reviews', headerTitleAlign: 'center', headerShown: false }} />
                 <Stack.Screen name="VisitShop" component={VisitShop} options={{ headerTitle: 'VisitShop', headerTitleAlign: 'center', headerShown: false }} />
             </Stack.Navigator>
