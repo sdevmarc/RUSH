8171f732b9c78a00296b770901e75d83453e9a9a
sdevmarc <suanieeee@yahoo.com>
2024-05-20 09:45:51 +0800

    refactor: Implement lazy loading for images in Sidebar.jsx

--- Frontend/Admin - Website/admin/src/components/Sidebar.jsx
+++ Frontend/Admin - Website/admin/src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import GridViewIcon from '@mui/icons-material/GridView'
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt'
 import SellIcon from '@mui/icons-material/Sell'
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'
 import StorefrontIcon from '@mui/icons-material/Storefront'
-import SettingsIcon from '@mui/icons-material/Settings'
 import './css/Sidebar.css'
+import { Button } from '@mui/material'
 
 const LinksTop = [
     { id: 1, linkto: '/dashboard', icon: <GridViewIcon />, name: 'Dashboard' },
@@ -16,11 +16,13 @@ const LinksTop = [
     { id: 5, linkto: '/shops', icon: <StorefrontIcon />, name: 'Shops' }
 ]
 
-const LinksBottom = [
-    { id: 1, linkto: '/settings', icon: <SettingsIcon />, name: 'Settings' }
-]
 
 export default function Sidebar() {
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        navigate('/')
+    }
     return (
         <>
             <div className="sidebar w-[17.5%] h-[91vh] border-r border-black p-[1rem] flex flex-col justify-between items-start gap-[1rem]">
@@ -37,18 +39,19 @@ export default function Sidebar() {
                         ))
                     }
                 </div>
-                <div className="w-full flex flex-col justify-start items-start gap-[.5rem]">
-                    {
-                        LinksBottom.map((item) => (
-                            <NavLink
-                                key={item.id}
-                                to={item.linkto}
-                                className={`w-full p-[.5rem] rounded-lg flex items-center gap-[.5rem] hover:bg-[#EFEFEF] duration-300 ease`}
-                            >
-                                {item.icon}{item.name}
-                            </NavLink>
-                        ))
-                    }
+                <div className="w-full flex flex-col justify-start items-start gap-[.3rem]">
+                    <Button variant="contained" sx={{ width: '100%', background: 'transparent', color: '#222', justifyContent: 'flex-start' }}>
+                        Backup
+                    </Button>
+                    <Button variant="contained" sx={{ width: '100%', background: 'transparent', color: '#222', justifyContent: 'flex-start' }}>
+                        Restore
+                    </Button>
+                    <Button
+                        onClick={handleLogout}
+                        variant="contained" sx={{ width: '100%', background: '#222', color: 'white', justifyContent: 'flex-start' }}
+                    >
+                        Logout
+                    </Button>
                 </div>
             </div>
         </>
