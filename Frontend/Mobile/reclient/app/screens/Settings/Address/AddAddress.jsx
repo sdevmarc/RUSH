c05eff49baf4e634b0b9c9a7d10dad3843608566
Marc Edison <suanieeee@yahoo.com>
2024-05-22 22:44:12 +0800

    refactor: Add validation for required fields in AddAddress
    Add validation for the required fields in the AddAddress component. If the lastname, firstname, middlename, or deliveryAddress fields are empty, display an error message. This change improves data integrity and provides a better user experience by preventing incomplete address submissions.

--- Frontend/Mobile/reclient/app/screens/Settings/Address/AddAddress.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/Address/AddAddress.jsx
@@ -55,8 +55,13 @@ const AddAddress = ({ route }) => {
     }
 
     const handleSubmit = async () => {
+        const { personalDetails, deliveryAddress } = values
+        const { lastname, firstname, middlename } = personalDetails
+
+        if (!lastname || !firstname || !middlename || deliveryAddress.length <= 0) return Alert.alert('Error', "Please fill in the required field!")
+
         const res = await axios.post(`http://${address}/api/addaddress`, values)
-       
+
         if (res.data.success) {
             Alert.alert(res.data.message)
             navigation.goBack()
