92872b7c245d3f317a8ffff6546e6ebe119dee76
Marc Edison <suanieeee@yahoo.com>
2024-05-23 00:12:17 +0800

    refactor: Update model name to 'Employees' in Admin.Users.js

--- Backend/server/models/Admin/Admin.Users.js
+++ Backend/server/models/Admin/Admin.Users.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
 const AdminUserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
     username: {
         type: String,
         required: true
@@ -12,15 +8,8 @@ const AdminUserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    isActive: {
-        type: String,
-        required: true
-    },
-    UserType: {
-        type: String
     }
 }, { timestamps: true })
 
-const AdminUserModel = mongoose.model('Users', AdminUserSchema)
+const AdminUserModel = mongoose.model('Employees', AdminUserSchema)
 module.exports = AdminUserModel
