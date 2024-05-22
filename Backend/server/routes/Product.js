55875d8fc9dae2ee1238129208c53c907e45ea4a
Marc Edison <suanieeee@yahoo.com>
2024-05-23 02:15:24 +0800

    refactor: Add GetAll and SearchAdminProduct routes to Product.js
    Add two new routes, '/getallproducts' and '/searchadminproduct/:searchId', to the Product.js file in the server/routes directory. These routes correspond to the newly added methods, GetAll and SearchAdminProduct, in the ProductController. This change enhances the functionality and flexibility of the product management system.

--- Backend/server/routes/Product.js
+++ Backend/server/routes/Product.js
@@ -5,8 +5,10 @@ const Products = require('../controllers/Products')
 
 router.post('/addproduct', Products.AddProduct)
 router.get('/getproducts/:storeId', Products.FetchProducts)
+router.get('/getallproducts', Products.GetAll)
 router.get('/selectproduct/:id/:IsDelivery', Products.SelectedItem)
 router.get('/searchproduct/:storeId/:searchId', Products.SearchProduct)
+router.get('/searchadminproduct/:searchId', Products.SearchAdminProduct)
 router.post('/updateproductdetails', Products.UpdateProductDetails)
 router.get('/deleteproduct/:productId', Products.DeleteProduct)
 
