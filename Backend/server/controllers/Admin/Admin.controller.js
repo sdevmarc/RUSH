1c23d898ca11df9de2f8738d59f0efc7dc6707c9
Marc Edison <suanieeee@yahoo.com>
2024-05-23 00:12:11 +0800

    refactor: Add Admin controller for user management
    Add a new Admin controller for user management in the Backend server. This controller includes functions for creating a user, getting all admin users, logging in an admin user, backing up the database, and restoring the database. This change improves the organization and structure of the server code, making it easier to manage and maintain.

---
+++ Backend/server/controllers/Admin/Admin.controller.js
@@ -0,0 +1,66 @@
+const AdminUsers = require('../../models/Admin/Admin.Users')
+const { exec } = require('child_process');
+const path = require('path');
+
+const dbName = 'rush';
+
+const mongodumpPath = '"C:\\Program Files\\MongoDB\\Tools\\100\\bin\\mongodump"';
+const mongorestorePath = '"C:\\Program Files\\MongoDB\\Tools\\100\\bin\\mongorestore"';
+
+const AdminUserController = {
+    CreateUser: async (req, res) => {
+        try {
+            const values = req.body
+
+            await AdminUsers.create(values)
+            res.json({ success: true, message: 'User added successfully!' })
+        } catch (error) {
+            res.json({ success: false, message: `Error adding user controller: ${error}` })
+        }
+    },
+    GetAllAdminUsers: async (req, res) => {
+        try {
+            const data = await AdminUsers.find()
+            res.json({ success: true, message: 'User added successfully!', data })
+        } catch (error) {
+            res.json({ success: false, message: `Error getting admin user controller: ${error}` })
+        }
+    },
+    LoginAdminUser: async (req, res) => {
+        try {
+
+        } catch (error) {
+            res.json({ success: false, message: `Error Login admin user controller: ${error}` })
+        }
+    },
+    BackUp: async (req, res) => {
+        try {
+            exec(`${mongodumpPath} --db ${dbName} --out ${dbName}_backups`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error executing command: ${error}`);
+                    return res.json({ success: false, message: 'Failed to backup database!' });
+                }
+                res.json({ success: true, message: 'Backup successfully!' });
+            });
+        } catch (error) {
+            res.json({ success: false, message: `Error Backup controller: ${error}` });
+        }
+    },
+
+    Restore: async (req, res) => {
+        try {
+            exec(`${mongorestorePath} backups`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error restoring backup: ${stderr}`);
+                    return res.json({ success: false, message: `Error restoring backup: ${stderr}` });
+                }
+                res.json({ success: true, message: 'Data restored successfully!' });
+            });
+        } catch (error) {
+            res.json({ success: false, message: `Error Restore controller: ${error}` });
+        }
+    }
+
+}
+
+module.exports = AdminUserController
