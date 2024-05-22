bdde4e794109789e3d120cb4ed96db8348b538ab
Marc Edison <suanieeee@yahoo.com>
2024-05-23 02:15:19 +0800

    refactor: Update CreateUser method in Admin controller
    Refactor the CreateUser method in the Admin controller to improve validation and error handling. This change adds checks for invalid characters in the username and ensures that the password meets the required criteria. Additionally, it checks if the username already exists before creating a new user. This update enhances the security and reliability of user creation functionality.

--- Backend/server/controllers/Admin/Admin.controller.js
+++ Backend/server/controllers/Admin/Admin.controller.js
@@ -1,6 +1,5 @@
 const AdminUsers = require('../../models/Admin/Admin.Users')
-const { exec } = require('child_process');
-const path = require('path');
+const { exec } = require('child_process')
 
 const dbName = 'rush';
 
@@ -10,9 +9,27 @@ const mongorestorePath = '"C:\\Program Files\\MongoDB\\Tools\\100\\bin\\mongores
 const AdminUserController = {
     CreateUser: async (req, res) => {
         try {
-            const values = req.body
+            const { username, password } = req.body
 
-            await AdminUsers.create(values)
+            const checkUsername = await AdminUsers.findOne({
+                username: username
+            })
+
+            const usernameInvalidChars = /[^a-zA-Z0-9_-]/
+            const passwordfCheck = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[0-9]).{8,}$/
+
+            if (usernameInvalidChars.test(username)) {
+                return res.json({ success: false, message: 'Username contains invalid characters. Only letters, numbers, underscores, and hyphens are allowed.' })
+            }
+
+            if (!passwordfCheck.test(password)) {
+                return res.json({ success: false, message: 'Password must be at least 8 characters long, contain at least one special character, and one number.' })
+            }
+
+            if (checkUsername) return res.json({ success: false, message: 'User already exists!' })
+
+
+            await AdminUsers.create({ username, password })
             res.json({ success: true, message: 'User added successfully!' })
         } catch (error) {
             res.json({ success: false, message: `Error adding user controller: ${error}` })
@@ -28,9 +45,66 @@ const AdminUserController = {
     },
     LoginAdminUser: async (req, res) => {
         try {
+            const { username, password } = req.body
+            const User = await AdminUsers.findOne({ username: username })
+
+            if (User) {
+                if (password === User.password) {
+                    res.json({ success: true, message: 'Login successful!' })
+                } else {
+                    res.json({ success: false, message: 'Username nor Password Incorrect!' });
+                }
+            } else {
+                res.json({ success: false, message: 'User does not exist!' })
+            }
+        } catch (error) {
+            return next(`Error Login Auth: ${error}`)
+        }
+    }, 
+    SearchUsers: async (req, res) => {
+        try {
+            const { searchId } = req.params
+
+            const checkusers = await AdminUsers.find({
+                username: { $regex: new RegExp(searchId, 'i') }
+            })
+
+            res.json({ success: true, message: 'Searching a user is successful!', data: checkusers })
 
         } catch (error) {
-            res.json({ success: false, message: `Error Login admin user controller: ${error}` })
+            res.json({ success: false, message: `Search user error from controller: ${error}` })
+        }
+    },
+    UpdateUser: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { username, password } = req.body;
+
+            const user = await AdminUsers.findById(id);
+            if (!user) {
+                return res.json({ success: false, message: 'User not found!' });
+            }
+
+            const usernameInvalidChars = /[^a-zA-Z0-9_-]/;
+            const passwordfCheck = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[0-9]).{8,}$/;
+
+            if (usernameInvalidChars.test(username)) {
+                return res.json({ success: false, message: 'Username contains invalid characters. Only letters, numbers, underscores, and hyphens are allowed.' });
+            }
+
+            if (!passwordfCheck.test(password)) {
+                return res.json({ success: false, message: 'Password must be at least 8 characters long, contain at least one special character, and one number.' });
+            }
+
+            const checkUsername = await AdminUsers.findOne({ username });
+            if (checkUsername && checkUsername._id.toString() !== id) {
+                return res.json({ success: false, message: 'Username already taken!' });
+            }
+
+            await AdminUsers.findByIdAndUpdate(id, { username, password });
+            res.json({ success: true, message: 'User updated successfully!' });
+        } catch (error) {
+            res.json({ success: false, message: `Error updating user: ${error}` });
         }
     },
     BackUp: async (req, res) => {
