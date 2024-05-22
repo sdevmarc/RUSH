fe448f68dca70cf797df93891e7862f74ad57adf
Marc Edison <suanieeee@yahoo.com>
2024-05-23 02:15:30 +0800

    feat: Add GetAllReport route to Reports.js

--- Backend/server/routes/Reports.js
+++ Backend/server/routes/Reports.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const Reports = require('../controllers/Reports')
 
 router.post('/addreport', Reports.AddReport)
+router.get('/getallreport', Reports.GetAllReport)
 
 module.exports = router
