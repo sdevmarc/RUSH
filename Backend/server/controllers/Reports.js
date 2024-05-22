8e6e1fff853d433a12b64363bc4ef74301c6db2c
Marc Edison <suanieeee@yahoo.com>
2024-05-23 02:15:08 +0800

    refactor: Add GetAllReport method to ReportsController

--- Backend/server/controllers/Reports.js
+++ Backend/server/controllers/Reports.js
@@ -16,6 +16,15 @@ const ReportsController = {
         } catch (error) {
             res.json({ success: false, message: `Error Add Report controller: ${error}`, error: error })
         }
+    },
+    GetAllReport: async (req, res) => {
+        try {
+            const data = await Reports.find()
+            res.json({ success: true, message: 'Fetched all reports!', data })
+
+        } catch (error) {
+            res.json({ success: false, message: `Error Get all Report controller: ${error}`, error: error })
+        }
     }
 }
 
