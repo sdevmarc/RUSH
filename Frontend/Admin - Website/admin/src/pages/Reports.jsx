239f81439d19c915208f97876898c2bf3181d7bb
Marc Edison <suanieeee@yahoo.com>
2024-05-23 02:17:42 +0800

    feat: Add Reports page to admin dashboard

---
+++ Frontend/Admin - Website/admin/src/pages/Reports.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import { DataGrid } from '@mui/x-data-grid'
+import address from '../config'
+import axios from 'axios'
+
+const columns = [
+    { field: 'id', headerName: 'No.', width: 70 },
+    { field: 'userId', headerName: 'User Id', width: 250 },
+    { field: 'subject', headerName: 'Subject', width: 230 },
+    { field: 'concern', headerName: 'Concern', width: 230 }
+];
+
+
+
+export default function Reports() {
+    const [values, setValues] = useState([])
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+        const res = await axios.get(`http://${address}/api/getallreport`)
+
+        const formattedData = res?.data?.data?.map((user) => ({
+            id: user._id,
+            userId: user.userId,
+            subject: user.subject,
+            concern: user.concern
+        }))
+        setValues(formattedData)
+    }
+
+
+    return (
+        <>
+            <div className="w-full h-screen">
+                <Navbar />
+                <div className="w-full h-[91%] flex">
+                    <Sidebar />
+                    <div className="w-[82.5%] h-full flex flex-col justify-between items-start p-[1rem]">
+                        <div className="w-full h-[8%] flex justify-between items-center ">
+                            <div className="h-full flex items-center gap-[1rem]">
+                                <h1 className='font-[600]'>Reports</h1>
+                            </div>
+                        </div>
+                        <div className="w-full h-[90%] bg-yellow flex justify-center items-center">
+                            <DataGrid
+                                rows={values}
+                                columns={columns}
+                                initialState={{
+                                    pagination: {
+                                        paginationModel: { page: 0, pageSize: 5 },
+                                    },
+                                }}
+                                pageSizeOptions={[5, 10]}
+                                checkboxSelection
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
