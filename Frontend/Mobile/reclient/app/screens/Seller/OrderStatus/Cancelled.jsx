105b83fac4a5e32a9cd085d9eb45e88f828d2377
Marc Edison <suanieeee@yahoo.com>
2024-06-04 03:05:10 +0800

    chore: Update API endpoint to use HTTPS and remove unused code in Cancelled.jsx

--- Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/Cancelled.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/Cancelled.jsx
@@ -11,7 +11,6 @@ import { useFocusEffect } from '@react-navigation/native'
 import * as Colors from '../../../../utils/colors'
 import Navbar from '../../../components/Navbar'
 import axios from 'axios'
-import address from '../../../../config/host'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Loading from '../../../components/Loading'
 
@@ -29,7 +28,7 @@ export default function Cancelled() {
         try {
             setIsLoading(true)
             const sellerId = await AsyncStorage.getItem('storeId')
-            const res = await axios.get(`${address}/api/viewstatustransactions/${sellerId}/seller/CANCELLED`)
+            const res = await axios.get(`${process.env.EXPO_PUBLIC_SERVER}/api/viewstatustransactions/${sellerId}/seller/CANCELLED`)
     
             if (res?.data?.success) {
                 setValues(res?.data?.data)
