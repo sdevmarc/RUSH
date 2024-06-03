c8ffd6b17b7d89eab897651770a43fe354d6c8e9
Marc Edison <suanieeee@yahoo.com>
2024-06-04 03:33:06 +0800

    chore: Update MongoDB connection to use environment variable

--- Backend/server/config/db.js
+++ Backend/server/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const connectDb = async () => {
     try {
-        // await mongoose.connect('mongodb://localhost:27017/rush')
         await mongoose.connect(process.env.MONGODB_URI)
         console.log('Connected to the database!')
     } catch (error) {
