f307ba9fa4033807c5655977e39f975616e4e84a
sdevmarc <suanieeee@yahoo.com>
2024-06-03 19:13:33 +0800

    Update http addrss to https

--- Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/Completed.jsx
+++ Frontend/Mobile/reclient/app/screens/Seller/OrderStatus/Completed.jsx
@@ -29,7 +29,7 @@ export default function Completed() {
         try {
             setIsLoading(true)
             const sellerId = await AsyncStorage.getItem('storeId')
-            const res = await axios.get(`http:${address}/api/viewstatustransactions/${sellerId}/seller/COMPLETED`)
+            const res = await axios.get(`${address}/api/viewstatustransactions/${sellerId}/seller/COMPLETED`)
 
             if (res?.data?.success) {
                 setValues(res?.data?.data)
