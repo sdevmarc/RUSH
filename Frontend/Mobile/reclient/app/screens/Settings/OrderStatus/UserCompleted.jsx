5adea2272120c1e1d91a1f9cb62228b1d3ee1aa8
Marc Edison <suanieeee@yahoo.com>
2024-06-04 03:06:23 +0800

    chore: Update API endpoint to use HTTPS and remove unused code in UserCompleted.jsx

--- Frontend/Mobile/reclient/app/screens/Settings/OrderStatus/UserCompleted.jsx
+++ Frontend/Mobile/reclient/app/screens/Settings/OrderStatus/UserCompleted.jsx
@@ -11,7 +11,6 @@ import { useFocusEffect } from '@react-navigation/native'
 import * as Colors from '../../../../utils/colors'
 import Navbar from '../../../components/Navbar'
 import axios from 'axios'
-import address from '../../../../config/host'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Loading from '../../../components/Loading'
 
@@ -29,7 +28,7 @@ export default function UserCompleted() {
         try {
             setIsLoading(true)
             const userId = await AsyncStorage.getItem('userId')
-            const res = await axios.get(`${address}/api/viewstatustransactions/${userId}/user/COMPLETED`)
+            const res = await axios.get(`${process.env.EXPO_PUBLIC_SERVER}/api/viewstatustransactions/${userId}/user/COMPLETED`)
 
             if (res?.data?.success) {
                 setValues(res?.data?.data)
